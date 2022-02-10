4a52f358cdd981204121ddd165a5704c90477960
tlugat <teo.lugat@hetic.net>
2022-02-10 12:31:42 +0100

    feat-API-7 : commons controller

--- api/services/Commons/controller.js
+++ api/services/Commons/controller.js
@@ -4,6 +4,7 @@ const { Op } = require('sequelize');
 
 
 module.exports = {
+    // Roles
     getRoles: function(res) {
         models.Roles.findAll({
             order: [['label', 'asc']]
@@ -13,4 +14,38 @@ module.exports = {
             .json({total_rows: results.length, data: results});
         })
     },
+    getRole: function(res, id){},
+    createRole: function(res, data){},
+    updateRole: function(res, id, data){},
+    deleteRole: function(res, id){},
+    // Pricings
+    getPricings: function(res){},
+    getPricing: function(res, id){},
+    createPricing: function(res, data){},
+    updatePricing: function(res, id, data){},
+    deletePricing: function(res, id){},
+    // Tags
+    getTags: function(res){},
+    getTag: function(res, id){},
+    createTag: function(res, data){},
+    updateTag: function(res, id, data){},
+    deleteTag: function(res, id){},
+    // Status
+    getStatus: function(res){},
+    getOneStatus: function(res, id){},
+    createStatus: function(res, data){},
+    updateStatus: function(res, id, data){},
+    deleteStatus: function(res, id){},
+    // Payments
+    getPayments: function(res){},
+    getPayment: function(res, id){},
+    createPayment: function(res, data){},
+    updatePayment: function(res, id, data){},
+    deletePayment: function(res, id){},
+    // Litigation_objects
+    getLitigation_objects: function(res){},
+    getLitigation_object: function(res, id){},
+    createLitigation_object: function(res, data){},
+    updateLitigation_object: function(res, id, data){},
+    deleteLitigation_object: function(res, id){},
 }
