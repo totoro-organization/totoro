92e05b39734e6b9f8d3aa0dd358c84793f945e2d
MaevaWolff <maeva.wolff@hetic.net>
2022-03-09 19:18:34 +0100

    feat(ui-mobileapp): add theme folder

---
+++ ui-mobileapp/theme/border.ts
@@ -0,0 +1,16 @@
+export const border = {
+  radius: {
+    none: `0px`,
+
+    // TODO: add our values (check figma)
+
+    circle: `50%`,
+    full: `9999px`, // for pill shapes
+  },
+
+  width: {
+    0: `0px`,
+    1: `1px`,
+    2: `2px`,
+  },
+};
---
+++ ui-mobileapp/theme/fonts.ts
@@ -0,0 +1,9 @@
+export const fonts = {
+  sizes: {
+    //  TODO: add all text sizes
+    md: "0.875rem", // default size based to 14px (refer to figma)
+    lg: "1rem",
+  },
+
+  //   TODO: add weights / line-height tokens / font-family (?)
+};
