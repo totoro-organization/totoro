74368077ab1f246ad962a28a1a29f282c368b7b8
MaevaWolff <maeva.wolff@hetic.net>
2022-03-10 16:22:33 +0100

    fix(ui-mobileapp): fix text node error on Button atom and add comment

--- ui-mobileapp/src/screens/Home.tsx
+++ ui-mobileapp/src/screens/Home.tsx
@@ -1,9 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Button, Text } from "react-native";
+import { Button as NativeButton, Text } from "react-native";
 import { AuthParamList } from "../navigation/StackNavigationParams";
 import { StackNavigationProp } from "@react-navigation/stack/";
 import MainLayout from "../components/layouts/MainLayout";
+import Button from "../components/atoms/Button";
 
 export default function Home() {
   const navigation = useNavigation<StackNavigationProp<AuthParamList>>();
@@ -12,15 +13,14 @@ export default function Home() {
     <MainLayout>
       <Text>Home</Text>
 
-      <Button
+      <NativeButton
         title="S'inscrire"
         onPress={() => navigation.navigate("S'inscrire")}
       />
 
-      <Button
-        title="Se connecter"
-        onPress={() => navigation.navigate("Se connecter")}
-      />
+      <Button onPress={() => navigation.navigate("Se connecter")}>
+        Se connecter
+      </Button>
     </MainLayout>
   );
 }
