9addc1f702a71b079e304f1421d610dbb5785f87
MaevaWolff <maeva.wolff@hetic.net>
2022-03-10 13:11:07 +0100

    fix(ui-mobileapp): remove useless code comment

--- ui-mobileapp/App.tsx
+++ ui-mobileapp/App.tsx
@@ -4,12 +4,6 @@ import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme/theme";
 import RootStackNavigator from "./src/navigation/RootStackNavigator";
 
-// NOTE: remove me later, this is an example to use styled-components.
-// const Container = styled(View)`
-//   background-color: ${({ theme }) => theme.colors.primary[500]};
-//   height: 100%;
-// `;
-
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
