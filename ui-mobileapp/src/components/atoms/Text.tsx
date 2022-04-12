3ed695bef3d2681d288a3f2d6b499c489569cece
MaevaWolff <maeva.wolff@hetic.net>
2022-04-12 03:13:39 +0200

    refactor(ui-mobileapp): create getColors function

--- ui-mobileapp/src/components/atoms/Text.tsx
+++ ui-mobileapp/src/components/atoms/Text.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components/native";
 import { fonts } from "../../theme/fonts";
+import theme from "../../theme/theme";
 
-export type Colors = "neutral" | "primary";
+export type Colors = "black" | "primary" | "secondary";
 // TODO: add all font weigth
 export type Weights = "regular";
 export type Sizes = keyof typeof fonts.sizes;
@@ -12,9 +13,20 @@ export type TextProps = {
   size?: Sizes;
 };
 
-// TODO: add color
+// TODO: add this function to utils file
+function getColors(color: Colors) {
+  switch (color) {
+    case "black":
+      return theme.colors.black;
+    case "primary":
+      return theme.colors.primary[500];
+    case "secondary":
+      return theme.colors.secondary[500];
+  }
+}
+
 export const Text = styled.Text<TextProps>`
-  color: inherit;
+  color: ${({ color }) => (color && getColors(color)) || "inherit"};
   font-weight: ${({ theme, weight }) =>
     weight ? theme.fonts.weight[weight] : theme.fonts.weight.regular};
 `;
