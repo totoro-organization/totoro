b701f2c4341f6e1e7c86afeb389811dcd3023244
tlugat <teo.lugat@hetic.net>
2022-04-14 16:01:00 +0200

    feat-BO: update title in different parts of the app / clean readme.md

--- ui-backoffice/src/content/pages/Status/Status500/index.tsx
+++ ui-backoffice/src/content/pages/Status/Status500/index.tsx
@@ -115,7 +115,7 @@ function Status500() {
               <Container maxWidth="sm">
                 <Box textAlign="center">
                   <TypographyPrimary variant="h1" sx={{ my: 2 }}>
-                    Tokyo Free White React Admin Dashboard
+                    Totoro Admin Dashboard
                   </TypographyPrimary>
                   <TypographySecondary
                     variant="h4"
