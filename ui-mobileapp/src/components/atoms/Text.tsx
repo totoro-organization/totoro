2df8a9fc81b6a7b586cf56963e517f44aeadd40a
MaevaWolff <maeva.wolff@hetic.net>
2022-04-15 19:34:24 +0200

    refactor(ui-mobileapp): replace black500 to grey9000 color

--- ui-mobileapp/src/components/atoms/Text.tsx
+++ ui-mobileapp/src/components/atoms/Text.tsx
@@ -17,7 +17,7 @@ export type TextProps = {
 function getColors(color: Colors) {
   switch (color) {
     case "black":
-      return theme.colors.black;
+      return theme.colors.grey[900];
     case "primary":
       return theme.colors.primary[500];
     case "secondary":
