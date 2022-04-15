ee088aa356faa86f96ed3c4479d3349abb7d94f1
MaevaWolff <maeva.wolff@hetic.net>
2022-04-15 19:32:38 +0200

    feat(ui-mobileapp): add grey shades to our theme

--- ui-mobileapp/src/theme/colors.ts
+++ ui-mobileapp/src/theme/colors.ts
@@ -15,9 +15,21 @@ export const colors = {
     600: "#FBFCFD",
   },
 
+  // TODO: Remove me and use grey900 instead
   black: {
     500: "#151515",
   },
 
-  //   grey: {}
+  grey: {
+    50: "#F9FAFB",
+    100: "#F3F4F6",
+    200: "#E5E7EB",
+    300: "#D2D5DA",
+    400: "#9DA3AE",
+    500: "#6C737F", // default
+    600: "#4D5662",
+    700: "#394250",
+    800: "#212936",
+    900: "#121826",
+  },
 };
