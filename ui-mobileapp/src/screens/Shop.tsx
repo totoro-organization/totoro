88baa171d3a681a8b8ffe73de75213e26172d5c1
MaevaWolff <maeva.wolff@hetic.net>
2022-04-17 18:30:39 +0200

    feat(ui-mobileapp): add shop page and remove publish page

--- ui-mobileapp/src/screens/Shop.tsx
+++ ui-mobileapp/src/screens/Shop.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Text } from "react-native";
 import MainLayout from "../components/layouts/MainLayout";
 
-export default function Publish() {
+export default function Shop() {
   return (
     <MainLayout>
-      <Text>Publish</Text>
+      <Text>Shop</Text>
     </MainLayout>
   );
 }
