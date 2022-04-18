036fdc18a1e58f0b48f6d076fe69896ce29d926f
MaevaWolff <maeva.wolff@hetic.net>
2022-04-18 03:42:01 +0200

    feat(ui-mobileapp): import Outfit font-family

--- ui-mobileapp/src/components/atoms/Text.tsx
+++ ui-mobileapp/src/components/atoms/Text.tsx
@@ -19,4 +19,5 @@ export const Text = styled.Text<TextProps>`
     weight ? theme.fonts.weight[weight] : theme.fonts.weight.regular};
   font-size: ${({ theme, size }) =>
     size ? theme.fonts.sizes[size] : theme.fonts.sizes.md};
+  font-family: "Outfit";
 `;
