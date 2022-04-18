f7cd13aac2ea370c5bbc206659da7bf37e757b42
MaevaWolff <maeva.wolff@hetic.net>
2022-04-18 04:14:00 +0200

    fix(ui-mobileapp): add Text tag aroung text to Loading page

--- ui-mobileapp/src/screens/Loading.tsx
+++ ui-mobileapp/src/screens/Loading.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { View } from "react-native";
 import Spinner from "../components/atoms/Spinner";
+import { Text } from "../components/atoms/Text";
 
 export default function Loading() {
   return (
     <View>
-      Loading
+      <Text>Loading</Text>
       <Spinner />
     </View>
   );
