8f872a998569deb62687122cb04f9e3733322094
MaevaWolff <maeva.wolff@hetic.net>
2022-04-18 05:04:20 +0200

    feat(ui-mobileapp): create explanation screen instead of homepage

--- ui-mobileapp/src/navigation/RootStackNavigator.tsx
+++ ui-mobileapp/src/navigation/RootStackNavigator.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
+import Explanation from "../screens/Explanation";
 import Home from "../screens/Home";
 import Login from "../screens/Login";
 import Register from "../screens/Register";
@@ -10,7 +11,7 @@ const RootStack = createStackNavigator();
 
 export default function RootStackNavigator() {
   // NOTE: Add isLogged with AuthProvider? AuthContext? etc.
-  const isLogged = true;
+  const isLogged = false;
 
   return (
     <RootStack.Navigator
@@ -21,7 +22,7 @@ export default function RootStackNavigator() {
     >
       {!isLogged && (
         <>
-          <RootStack.Screen name="Accueil" component={Home} />
+          <RootStack.Screen name="Explications" component={Explanation} />
           <RootStack.Screen name="Se connecter" component={Login} />
           <RootStack.Screen name="S'inscrire" component={Register} />
         </>
