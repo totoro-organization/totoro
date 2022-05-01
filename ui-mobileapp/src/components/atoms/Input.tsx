e75bdfb089654058557faa75559e07f4e0df8ef0
MaevaWolff <maeva.wolff@hetic.net>
2022-05-01 18:12:18 +0200

    feat(ui-mobileapp): add focus input style

--- ui-mobileapp/src/components/atoms/Input.tsx
+++ ui-mobileapp/src/components/atoms/Input.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TextInputProps } from "react-native";
 import styled from "styled-components/native";
 
@@ -7,26 +8,31 @@ export default function Input({
   error = false,
   ...nativeTextInputProps
 }: InputProps) {
-  return <StyledInput $error={error} {...nativeTextInputProps} />;
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  return (
+    <StyledInput
+      $error={error}
+      $active={isActive}
+      {...nativeTextInputProps}
+      onFocus={() => setIsActive(true)}
+      onBlur={() => setIsActive(false)}
+    />
+  );
 }
 
-const StyledInput = styled.TextInput<{ $error: boolean }>`
-  padding: 14px;
+const StyledInput = styled.TextInput<{ $error: boolean; $active: boolean }>`
+  padding: ${({ theme }) => theme.spacing[3.5]};
   border: ${({ theme }) => theme.border.width[1]} solid
-    ${({ theme, $error }) => ($error ? "red" : theme.colors.grey[200])};
+    ${({ theme, $error, $active }) =>
+      $error
+        ? "red"
+        : $active
+        ? theme.colors.grey[900]
+        : theme.colors.grey[200]};
   border-radius: ${({ theme }) => theme.border.radius.md};
   background-color: ${({ theme, $error }) =>
     $error ? theme.colors.primary[50] : theme.colors.grey[50]};
   color: ${({ theme }) => theme.colors.grey[900]};
   font-family: "Outfit";
-
-  /* NOTE: Box-shadow on react-native app. */
-  shadow-opacity: 0.75;
-  shadow-radius: 4px;
-  shadow-color: ${({ $error }) => ($error ? "red" : "transparent")};
-  shadow-offset: 0px 0px;
-
-  &:focus-visible {
-    border: 1px solid blue;
-  }
 `;
