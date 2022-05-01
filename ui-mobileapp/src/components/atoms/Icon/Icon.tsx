21f6041f1cc2b238cb435e824ee6044158ea6165
MaevaWolff <maeva.wolff@hetic.net>
2022-05-01 19:46:48 +0200

    refactor(ui-mobileapp): switch em to px on all icons

--- ui-mobileapp/src/components/atoms/Icon/Icon.tsx
+++ ui-mobileapp/src/components/atoms/Icon/Icon.tsx
@@ -37,8 +37,7 @@ export default function Icon({ name, color, size, ...rest }: IconProps) {
 }
 
 const Wrapper = styled.View<Pick<IconProps, "color" | "size">>`
-  font-size: ${({ size }) => (size && `${size}em`) || "24px"};
-  /* width: fit-content; */
+  font-size: ${({ size }) => (size && `${size * 16}px`) || "24px"};
   color: ${({ color, theme }) =>
     (color && getColors(color)) || theme.colors.grey[400]};
 `;
