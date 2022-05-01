28d046a0bd0d7d5ce033b907c532332a4dde718e
MaevaWolff <maeva.wolff@hetic.net>
2022-05-01 17:57:29 +0200

    feat(ui-mobileapp): add Spacer atom

---
+++ ui-mobileapp/src/components/atoms/Spacer.tsx
@@ -0,0 +1,27 @@
+// NOTE: Spacer component inspired by Josh Comeau's snippet (https://www.joshwcomeau.com/snippets/react-components/spacer/)
+
+import styled from "styled-components/native";
+
+type Props = {
+  axis: "horizontal" | "vertical";
+  size: number;
+};
+
+function getHeight({ axis, size }: Props) {
+  return axis === "horizontal" ? "1px" : `${size * 16}px`;
+}
+
+function getWidth({ axis, size }: Props) {
+  return axis === "vertical" ? "1px" : `${size * 16}px`;
+}
+
+/**
+ * @param size expressed in em units (1em = 16px)
+ * @param axis 'horizontal' | 'vertical' is the direction of spacing
+ */
+export default styled.View`
+  width: ${getWidth};
+  min-width: ${getWidth};
+  height: ${getHeight};
+  min-height: ${getHeight};
+`;
