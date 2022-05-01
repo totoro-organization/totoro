763c73d13896647c8f56d07e6b99a33dbf1ac11c
MaevaWolff <maeva.wolff@hetic.net>
2022-05-01 22:50:20 +0200

    feat(ui-mobileapp): add arrow svg icon

---
+++ ui-mobileapp/src/assets/icons/Arrow.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+
+export default function Arrow() {
+  return (
+    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <Path
+        d="M9.57 5.93005L3.5 12.0001L9.57 18.0701"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <Path
+        d="M20.4999 12H3.66992"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+}
