5233e4d5b3b15ed66162fdd77f0955b271ed8179
MaevaWolff <maeva.wolff@hetic.net>
2022-05-01 22:54:16 +0200

    refactor(ui-mobileapp): set icons width/height to 16px

--- ui-mobileapp/src/assets/icons/Arrow.tsx
+++ ui-mobileapp/src/assets/icons/Arrow.tsx
@@ -3,7 +3,7 @@ import Svg, { Path } from "react-native-svg";
 
 export default function Arrow() {
   return (
-    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <Svg width="16px" height="16px" viewBox="0 0 24 24" fill="none">
       <Path
         d="M9.57 5.93005L3.5 12.0001L9.57 18.0701"
         stroke="currentColor"
