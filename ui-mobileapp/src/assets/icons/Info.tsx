9b59cb5f1887e662a0d458423921fb3a076f1b27
MaevaWolff <maeva.wolff@hetic.net>
2022-05-01 22:54:16 +0200

    refactor(ui-mobileapp): set icons width/height to 16px

--- ui-mobileapp/src/assets/icons/Info.tsx
+++ ui-mobileapp/src/assets/icons/Info.tsx
@@ -3,7 +3,7 @@ import Svg, { Path, Circle } from "react-native-svg";
 
 export default function Info() {
   return (
-    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <Svg width="16px" height="16px" viewBox="0 0 24 24" fill="none">
       <Path
         d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
         stroke="currentColor"
