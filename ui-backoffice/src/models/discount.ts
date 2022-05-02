4511a4aea3560c5093740829045728bcbc20c6bb
tlugat <teo.lugat@hetic.net>
2022-05-02 15:20:15 +0200

    feat-BO-models: discount + discount_transaction + discount_type

--- ui-backoffice/src/models/discount.ts
+++ ui-backoffice/src/models/discount.ts
@@ -1,9 +1,18 @@
+import { DiscountType } from "./discount_type";
+import { Partner } from "./partner";
 import { Status } from "./status";
 
 export type DiscountStatus = Status<'active' | 'inactive' | 'unavailable'>;
 
 export interface Discount {
     id: string,
-    label: string,
+    name: string,
+    type: DiscountType,
+    description: string,
+    condition: string,
+    partner: Partner,
+    cost: number,
     status: DiscountStatus
+    createdAt: string,
+    updatedAt: string
 }
