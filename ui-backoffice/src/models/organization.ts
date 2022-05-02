efec91aa856809dee4c2b26f697509c9b5dabcfe
tlugat <teo.lugat@hetic.net>
2022-05-02 14:11:37 +0200

    feat-BO-models: organization

--- ui-backoffice/src/models/organization.ts
+++ ui-backoffice/src/models/organization.ts
@@ -6,9 +6,21 @@ export type OrganizationStatus = Status<'active' | 'inactive' | 'freezed' | 'out
 export interface Organization {
     id: string,
     name: string,
-    email: string,
-    address: string,
-    jobs: Job[],
-    phone: string,
-    status: OrganizationStatus
+    email?: string,
+    address?: string,
+    cp?: number,
+    commune?: string,
+    logo: string,
+    description?: string,
+    link?: string,
+    phone?: string,
+    siren: number,
+    siret: number,
+    longitude: number,
+    latitude: number,
+    status: OrganizationStatus,
+    creation_date?: string,
+    activity?: string,
+    createdAt: string,
+    updatedAt: string
 }
