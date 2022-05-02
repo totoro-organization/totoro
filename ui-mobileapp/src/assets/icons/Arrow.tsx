56ede64ebf169e075fe94b7f18b5e88123a783f2
MaevaWolff <maeva.wolff@hetic.net>
2022-05-02 14:42:28 +0200

    fix(ui-mobileapp): fix color prop on all svg icons

--- ui-mobileapp/src/assets/icons/Arrow.tsx
+++ ui-mobileapp/src/assets/icons/Arrow.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 import Svg, { Path } from "react-native-svg";
-import { Colors } from "../../theme/utils";
+import theme from "../../theme/theme";
 
 type ArrowProps = {
-  color?: Colors;
+  color?: keyof typeof theme.colors;
   size?: number;
 };
 
@@ -13,7 +13,7 @@ export default function Arrow({ color = "grey", size = 16 }: ArrowProps) {
     <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
       <Path
         d="M9.57 5.93005L3.5 12.0001L9.57 18.0701"
-        stroke={color}
+        stroke={theme.colors[color][500]}
         stroke-width="1.5"
         stroke-miterlimit="10"
         stroke-linecap="round"
@@ -21,7 +21,7 @@ export default function Arrow({ color = "grey", size = 16 }: ArrowProps) {
       />
       <Path
         d="M20.4999 12H3.66992"
-        stroke={color}
+        stroke={theme.colors[color][500]}
         stroke-width="1.5"
         stroke-miterlimit="10"
         stroke-linecap="round"
