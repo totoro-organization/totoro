0f146b52c7c2cfb839323079a3c3408a906f0d32
tlugat <teo.lugat@hetic.net>
2022-05-02 14:27:20 +0200

    feat-BO-models: admin + log

--- ui-backoffice/src/models/admin.ts
+++ ui-backoffice/src/models/admin.ts
@@ -1,22 +1,20 @@
+import { Log } from "./Log";
+import { Role } from "./role";
 import { Status } from "./status";
-import { Tag } from "./tag";
 
 export type AdminStatus = Status<'completed' | 'pending' | 'coming'>;
 
 export interface Admin {
     id: string,
-    title: string,
-    organization: string,
-    participants: number,
-    address: string,
-    capacity: number,
-    status:AdminStatus,
-    tokens: number,
-    desc?: string,
-    date: number,
-    tags?: Tag[],
-    banner?: string,
-    logo?: string,
+    status: AdminStatus,
+    role: Role,
+    firsname: string,
+    lastname: string,
+    username: string,
+    email: string,
+    logs: Log[],
+    createdAt: string,
+    updatedAt: string
 }
 
 
