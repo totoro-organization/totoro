306294f6e3ed79f505ec3d3a4febaf3f454295c0
MaevaWolff <maeva.wolff@hetic.net>
2022-05-07 17:26:47 +0200

    refactor(ui-mobileapp): use spacing tokens to Explanation screen

--- ui-mobileapp/src/screens/Explanation.tsx
+++ ui-mobileapp/src/screens/Explanation.tsx
@@ -5,7 +5,6 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthParamList } from "../navigation/StackNavigationParams";
 import Button from "../components/atoms/Button";
-import { Text as NativeText } from "react-native";
 
 export default function Explanation() {
   const navigation = useNavigation<StackNavigationProp<AuthParamList>>();
@@ -28,16 +27,15 @@ export default function Explanation() {
 }
 
 const Container = styled.View`
-  display: flex;
   align-items: center;
   justify-content: center;
-  padding: 16px;
+  padding: ${({ theme }) => theme.spacing[4]};
   width: 100%;
   height: 100%;
   background-color: ${({ theme }) => theme.colors.grey[900]};
 `;
 
 const WhiteText = styled(Text)`
-  margin-top: 16px;
+  margin-top: ${({ theme }) => theme.spacing[4]};
   color: ${({ theme }) => theme.colors.white[600]};
 `;
