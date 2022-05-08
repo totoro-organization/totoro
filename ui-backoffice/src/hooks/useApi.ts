f8f88657f4e6c877a2877ace969df0aedafbea6c
tlugat <teo.lugat@hetic.net>
2022-05-08 13:28:21 +0200

    feat-bo-api: add custom useApi hook

---
+++ ui-backoffice/src/hooks/useApi.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+export type ApiResponse = {
+  status: Number;
+  statusText: String;
+  data: any;
+  error: any;
+  loading: Boolean;
+};
+
+export const useApiGet = (url: string): ApiResponse => {
+  const [status, setStatus] = useState<Number>(0);
+  const [statusText, setStatusText] = useState<String>('');
+  const [data, setData] = useState<any>();
+  const [error, setError] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const getAPIData = async () => {
+    setLoading(true);
+    try {
+      const apiResponse = await fetch(url);
+      const json = await apiResponse.json();
+      setStatus(apiResponse.status);
+      setStatusText(apiResponse.statusText);
+      setData(json);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getAPIData();
+  }, []);
+
+  return { status, statusText, data, error, loading };
+};
