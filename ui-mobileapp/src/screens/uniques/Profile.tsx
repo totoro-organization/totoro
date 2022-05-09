91f3f8928cbb4f4fc8ec8b4fdb6607c7219d1b11
MaevaWolff <maeva.wolff@hetic.net>
2022-05-09 21:41:40 +0200

    refactor(ui-mobileapp): add following button to organization profile

--- ui-mobileapp/src/screens/uniques/Profile.tsx
+++ ui-mobileapp/src/screens/uniques/Profile.tsx
@@ -1,5 +1,7 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
+import Button from "../../components/atoms/Button";
+import Spacer from "../../components/atoms/Spacer";
 import { Text } from "../../components/atoms/Text";
 import MainLayout from "../../components/layouts/MainLayout";
 import { StackParamList } from "../../navigation/StackNavigationParams";
@@ -9,14 +11,28 @@ export type ProfileType = "user" | "organization";
 export default function Profile({
   route,
 }: StackScreenProps<StackParamList, "Profile">) {
+  const paramId = route.params.id;
   const type = route.params.type;
 
-  const isUserProfile = type === "user";
-  const isOrganizationProfile = type === "organization";
+  const isUser = type === "user";
+  const isOrganization = type === "organization";
 
   return (
     <MainLayout>
       <Text>{type} profile page</Text>
+
+      <Spacer axis="vertical" size={1} />
+
+      {isOrganization && (
+        // TODO: Add function to follow organization with call api
+        <Button
+          size="sm"
+          handlePress={() => console.log("suivi !")}
+          color="black"
+        >
+          Suivre
+        </Button>
+      )}
     </MainLayout>
   );
 }
