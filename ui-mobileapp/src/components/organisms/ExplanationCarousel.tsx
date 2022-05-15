227375ca1072dd9163ec24cd4347a9f37266388d
MaevaWolff <maeva.wolff@hetic.net>
2022-05-15 21:37:03 +0200

    feat(ui-mobileapp): add images to explanation carousel

--- ui-mobileapp/src/components/organisms/ExplanationCarousel.tsx
+++ ui-mobileapp/src/components/organisms/ExplanationCarousel.tsx
@@ -83,14 +83,17 @@ const Dot = styled.View<{ $active?: boolean }>`
 // DATA
 const explanationSteps = [
   {
+    image: require(`../../assets/illustrations/MissionIllustration.png`),
     title: "Participes à des missions en bénévolat.",
     text: "Totoro te propose de participer à des actions proposées par les associations le plus proche de chez toi.",
   },
   {
+    image: require(`../../assets/illustrations/EarnTokensIllustration.png`),
     title: "Sois récompensé-e avec notre système de tokens.",
     text: "Dès qu’une de tes missions est terminée, tu peux récupérer des tokens grâce au code de validation liée à la mission !",
   },
   {
+    image: require(`../../assets/illustrations/DiscountCouponsIllustration.png`),
     title: "Viens convertir tes tokens en bons de réduction !",
     text: "Totoro est en partenariat avec des boutiques locales de ton secteur, tu peux donc échanger tes tokens pour des bons de réduction ou avantages.",
   },
