379a20cb7e73a5a1c3e06cca6a7f89a21fcd7a66
MaevaWolff <maeva.wolff@hetic.net>
2022-05-16 22:56:02 +0200

    feat(ui-mobileapp): add config file and add auth api routes

---
+++ mobileapp/src/common/api/routes.ts
@@ -0,0 +1,7 @@
+// TODO: use .env
+export const API_HOST = "http://127.0.0.1:6868/api";
+
+export const API_ROUTES = {
+  AUTH_LOGIN: "/auth/login", // POST
+  AUTH_REGISTER: "/auth/signup", // POST
+} as const;
