4402f7b018ba8f0d5cec6feb161e953a0ac7597d
MaevaWolff <maeva.wolff@hetic.net>
2022-05-20 01:50:16 +0200

    refactor(api): improve readability and add spaces on all services files

--- api/src/services/Authentications/index.js
+++ api/src/services/Authentications/index.js
@@ -8,34 +8,35 @@ const controller = require("./controller");
 const { Users, Admins } = require("../../../models");
 
 exports.router = (function () {
-	const authRouter = express.Router();
+  const authRouter = express.Router();
 
-	authRouter.post("/login", async function (req, res) {
-		const data = req.body;
-		controller.login(res, Users, data, false);
-	});
+  authRouter.post("/login", async function (req, res) {
+    const data = req.body;
+    controller.login(res, Users, data, false);
+  });
 
-	authRouter.post("/login/admin", async function (req, res) {
-		const data = req.body;
-		controller.login(res, Admins, data, true);
-	});
+  authRouter.post("/login/admin", async function (req, res) {
+    const data = req.body;
+    controller.login(res, Admins, data, true);
+  });
 
-	authRouter.post("/signup", async function (req, res) {
-		const data = req.body;
-		controller.signup(res, Users, data);
-	});
+  authRouter.post("/signup", async function (req, res) {
+    const data = req.body;
+    controller.signup(res, Users, data);
+  });
 
-	authRouter.post("/forgot", async function (req, res) {
-		const data = req.body;
-		controller.forgot(res, Users, data);
-	});
+  authRouter.post("/forgot", async function (req, res) {
+    const data = req.body;
+    controller.forgot(res, Users, data);
+  });
 
-	authRouter.get("/connected",[passport, async function (req, res) {
-		if(req.userData.isAdmin)
-			adminsController.getAdmin(res, req.userData.id);
-		else
-			usersController.getUser(res, req.userData.id);
-	}]);
+  authRouter.get("/connected", [
+    passport,
+    async function (req, res) {
+      if (req.userData.isAdmin) adminsController.getAdmin(res, req.userData.id);
+      else usersController.getUser(res, req.userData.id);
+    },
+  ]);
 
-	return authRouter;
+  return authRouter;
 })();
