a77d45bf09c336a9a8664f2528d510377b524dd8
tlugat <teo.lugat@hetic.net>
2022-05-24 16:23:34 +0200

    feat-bo-logo: add new logo

--- backoffice/src/components/Logo/index.tsx
+++ backoffice/src/components/Logo/index.tsx
@@ -1,12 +1,14 @@
-import { Box, Hidden, Tooltip } from '@mui/material';
+import { Box, Hidden } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
+import logo from 'src/assets/image/logo/totoro-logo.svg';
 
 const LogoWrapper = styled(Link)(
   ({ theme }) => `
         color: ${theme.palette.text.primary};
         padding: ${theme.spacing(0, 1, 0, 0)};
         display: flex;
+        flex-direction: column;
         text-decoration: none;
         font-weight: ${theme.typography.fontWeightBold};
 `
@@ -18,75 +20,6 @@ const LogoSignWrapper = styled(Box)(
         height: 38px;
         margin-top: 4px;
         transform: scale(.8);
-`
-);
-
-const LogoSign = styled(Box)(
-  ({ theme }) => `
-        background: ${theme.general.reactFrameworkColor};
-        width: 18px;
-        height: 18px;
-        border-radius: ${theme.general.borderRadiusSm};
-        position: relative;
-        transform: rotate(45deg);
-        top: 3px;
-        left: 17px;
-
-        &:after, 
-        &:before {
-            content: "";
-            display: block;
-            width: 18px;
-            height: 18px;
-            position: absolute;
-            top: -1px;
-            right: -20px;
-            transform: rotate(0deg);
-            border-radius: ${theme.general.borderRadiusSm};
-        }
-
-        &:before {
-            background: ${theme.palette.primary.main};
-            right: auto;
-            left: 0;
-            top: 20px;
-        }
-
-        &:after {
-            background: ${theme.palette.secondary.main};
-        }
-`
-);
-
-const LogoSignInner = styled(Box)(
-  ({ theme }) => `
-        width: 16px;
-        height: 16px;
-        position: absolute;
-        top: 12px;
-        left: 12px;
-        z-index: 5;
-        border-radius: ${theme.general.borderRadiusSm};
-        background: ${theme.header.background};
-`
-);
-
-const LogoTextWrapper = styled(Box)(
-  ({ theme }) => `
-        padding-left: ${theme.spacing(1)};
-`
-);
-
-const VersionBadge = styled(Box)(
-  ({ theme }) => `
-        background: ${theme.palette.success.main};
-        color: ${theme.palette.success.contrastText};
-        padding: ${theme.spacing(0.4, 1)};
-        border-radius: ${theme.general.borderRadiusSm};
-        text-align: center;
-        display: inline-block;
-        line-height: 1;
-        font-size: ${theme.typography.pxToRem(11)};
 `
 );
 
@@ -103,17 +36,10 @@ function Logo() {
   return (
     <LogoWrapper to="/dashboards/statistiques">
       <LogoSignWrapper>
-        <LogoSign>
-          <LogoSignInner />
-        </LogoSign>
+          <img src={logo} alt="" />
       </LogoSignWrapper>
       <Hidden smDown>
-        <LogoTextWrapper>
-          <Tooltip title="Version 1.1.0" arrow placement="right">
-            <VersionBadge>1.1</VersionBadge>
-          </Tooltip>
-          <LogoText>Totoro Admin</LogoText>
-        </LogoTextWrapper>
+        <LogoText>Admin Dashboard</LogoText>
       </Hidden>
     </LogoWrapper>
   );
