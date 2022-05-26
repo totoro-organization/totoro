a9f11f347b60a1892601d1879bfbdaba29339cc0
tlugat <teo.lugat@hetic.net>
2022-05-26 17:13:49 +0200

    feat-bo: add Modal / refacto Tags table using TableWrapper / add tags service and add edit method to TagsTable

--- backoffice/src/pages/AppsConfiguration/Settings/Tags/Tags.tsx
+++ backoffice/src/pages/AppsConfiguration/Settings/Tags/Tags.tsx
@@ -3,16 +3,26 @@ import { Card } from '@mui/material';
 import TagsTable from './TagsTable';
 import { useApi } from 'src/hooks/useApi';
 import SuspenseLoader from 'src/components/SuspenseLoader';
+import TableWrapper from 'src/components/TableWrapper';
+import { useState } from 'react';
 
 function Tags() {
 
-  const { data: users, loading } = useApi('/users');
+  const { data: tags, loading  } = useApi('/commons/tags');
+
+  const [mustReload, setMustReload] = useState(false);
+
+  const handleReload = () => {
+    setMustReload(!mustReload);
+  }
 
   return (
     <Card>
       {
         loading ? <SuspenseLoader/> :
-        <TagsTable users={users?.data} />
+        <TableWrapper items={tags?.data}>
+            <TagsTable handleReload={handleReload} />
+        </TableWrapper>
       }
     </Card>
   );
