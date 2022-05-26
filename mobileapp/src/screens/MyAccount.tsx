3d440208a77629667e864fe3747a9d3c39984a84
MaevaWolff <maeva.wolff@hetic.net>
2022-05-26 16:31:22 +0200

    refactor(mobileapp): add logout button to MyAccount screen

--- mobileapp/src/screens/MyAccount.tsx
+++ mobileapp/src/screens/MyAccount.tsx
@@ -1,11 +1,18 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
+import useAuth from "../common/contexts/AuthContext";
+import Button from "../components/atoms/Button";
 import { Text } from "../components/atoms/Text";
 import MainLayout from "../components/layouts/MainLayout";
 
 export default function MyAccount() {
+  const { logout } = useAuth();
+
   return (
     <MainLayout>
       <Text>My Account</Text>
+
+      <Button handlePress={logout}>Se déconnecter</Button>
     </MainLayout>
   );
 }
