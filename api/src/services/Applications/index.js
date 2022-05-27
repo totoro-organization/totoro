7fbfeb636ab01b38ba8ca4ebc68aff33991bae61
unknown <jarce.boukoro@korian.fr>
2022-05-27 12:09:12 +0200

    add routes commons and update partners

--- api/src/services/Applications/index.js
+++ api/src/services/Applications/index.js
@@ -2,36 +2,37 @@ const express = require("express");
 const { passport } = require("utils/session");
 const controller = require("services/Commons/controller");
 const { Applications } = require("./../../../models");
+const { passportAdmin } = require("utils/session");
 
 exports.router = (function () {
   const applicationsRouter = express.Router();
 
-  applicationsRouter.get("/", async function (req, res) {
+  applicationsRouter.get("/", [passportAdmin, async function (req, res) {
     controller.getAll(res, Applications);
-  });
+  }]);
 
-  applicationsRouter.get("/:id", async function (req, res) {
+  applicationsRouter.get("/:id",[passportAdmin, async function (req, res) {
     const id = req.params.id;
     controller.getOne(res, Applications, id);
-  });
+  }]);
 
-  applicationsRouter.post("/", async function (req, res) {
+  applicationsRouter.post("/", [passportAdmin, async function (req, res) {
     const data = req.body;
     const condition = { name: data.name };
     controller.create(null, res, Applications, data, condition);
-  });
+  }]);
 
-  applicationsRouter.put("/:id", async function (req, res) {
+  applicationsRouter.put("/:id",[passportAdmin, async function (req, res) {
     const id = req.params.id;
     const data = req.body;
     const condition = { name: data.name };
     controller.update(res, Applications, id, data, condition);
-  });
+  }]);
 
-  applicationsRouter.delete("/:id", async function (req, res) {
+  applicationsRouter.delete("/:id",[passportAdmin, async function (req, res) {
     const id = req.params.id;
     controller.delete(res, Applications, { id });
-  });
+  }]);
 
   return applicationsRouter;
 })();
