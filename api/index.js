d2923d632295580df060c99a8d398fc907a73566
tlugat <teo.lugat@hetic.net>
2022-05-28 19:53:36 +0200

    feat-api: getJobs and getJob routes

--- api/index.js
+++ api/index.js
@@ -9,12 +9,13 @@ const swaggerTools = require("swagger-tools");
 let swaggerDoc = require("./swagger.json");
 
 const {
-  users,
-  admins,
-  terminals,
-  authentications,
-  commons,
-  applications,
+	users,
+	admins,
+	terminals,
+	authentications,
+	commons,
+	applications,
+	ads,
 } = require("services");
 
 const PORT = process.env.API_DOCKER_PORT || 8080;
@@ -27,32 +28,32 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
 const accessApi = async (req, res, next) => {
-  try {
-    const APP_ID = req.headers["app_id"];
+	try {
+		const APP_ID = req.headers["app_id"];
 
-    if (!APP_ID) {
-      return res
-        .status(error.access_denied.status)
-        .json({ message: error.access_denied.message });
-    }
+		if (!APP_ID) {
+			return res
+				.status(error.access_denied.status)
+				.json({ message: error.access_denied.message });
+		}
 
-    Applications.findOne({
-      where: { id: APP_ID },
-    })
-      .then((data) => {
-        if (data) {
-          req.request = data;
-          next();
-        } else {
-          return res
-            .status(error.access_denied.status)
-            .json({ message: error.access_denied.message });
-        }
-      })
-      .catch((error) => console.log(error));
-  } catch (error) {
-    console.log("jarce");
-  }
+		Applications.findOne({
+			where: { id: APP_ID },
+		})
+			.then((data) => {
+				if (data) {
+					req.request = data;
+					next();
+				} else {
+					return res
+						.status(error.access_denied.status)
+						.json({ message: error.access_denied.message });
+				}
+			})
+			.catch((error) => console.log(error));
+	} catch (error) {
+		console.log("jarce");
+	}
 };
 
 server.use("/api/applications", [accessApi, applications]);
@@ -61,7 +62,7 @@ server.use("/api/terminals", [accessApi, terminals]);
 server.use("/api/auth", [accessApi, authentications]);
 server.use("/api/users", [accessApi, users]);
 server.use("/api/admins", [accessApi, admins]);
-// server.use('/api/ads', [accessApi, ads]);
+server.use("/api/ads", [accessApi, ads]);
 // server.use('/api/messagings', [accessApi, messagings]);
 // server.use('/api/transactions', [accessApi, transactions]);
 // server.use('/api/litigations', [accessApi, litigations]);
@@ -69,16 +70,16 @@ server.use("/api/admins", [accessApi, admins]);
 server.use("/api/commons", [accessApi, commons]);
 
 const options = {
-  controllers: "./src/services",
+	controllers: "./src/services",
 };
 
 swaggerTools.initializeMiddleware(swaggerDoc, (middleware) => {
-  server.use(middleware.swaggerMetadata());
-  server.use(middleware.swaggerValidator());
-  server.use(middleware.swaggerRouter(options));
-  server.use(middleware.swaggerUi());
+	server.use(middleware.swaggerMetadata());
+	server.use(middleware.swaggerValidator());
+	server.use(middleware.swaggerRouter(options));
+	server.use(middleware.swaggerUi());
 
-  server.listen(PORT, function () {
-    console.log("server start");
-  });
+	server.listen(PORT, function () {
+		console.log("server start");
+	});
 });
