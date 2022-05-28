217414fd7b5e9fb9e2cf1432cf6367a713806f3f
tlugat <teo.lugat@hetic.net>
2022-05-28 19:53:36 +0200

    feat-api: getJobs and getJob routes

--- api/src/services/Ads/controller.js
+++ api/src/services/Ads/controller.js
@@ -1,11 +1,27 @@
 const models = require("./../../../models");
 const asyncLib = require("async");
 const { Op } = require("sequelize");
+const commonsController = require("services/Commons/controller");
+const { getRow } = require("~/utils/common/thenCatch");
+const { Ads, Status } = require("./../../../models");
 
 module.exports = {
-  getAds: function (res) {},
-  getAd: function (res, id) {},
-  createAd: function (res, data) {},
-  updateAd: function (res, id, data) {},
-  deleteAd: function (res, id) {},
+	getAds: async function (res, queries = null) {
+		let condition = {};
+		if (queries && queries.status) {
+			let statusData = await getRow(res, Status, { label: queries.status });
+			condition.status_id = statusData.id;
+		}
+
+		condition = Object.keys(condition).length === 0 ? null : condition;
+		commonsController.getAll(res, Ads, condition);
+	},
+	getAd: function (res, id) {
+		commonsController.getOne(res, Ads, id);
+	},
+	createAd: function (res, data) {},
+	updateAd: function (res, id, data) {},
+	deleteAd: function (res, id) {
+		commonsController.delete(res, Ads, { id });
+	},
 };
--- api/src/services/Ads/index.js
+++ api/src/services/Ads/index.js
@@ -1,34 +1,43 @@
 const express = require("express");
-const { passport } = require("utils/session");
+const { passport, passportAdmin } = require("utils/session");
 const controller = require("./controller");
 
 exports.router = (function () {
-  const adsRouter = express.Router();
+	const adsRouter = express.Router();
 
-  adsRouter.get("/", async function (req, res) {
-    controller.getAds(res);
-  });
+	adsRouter.get("/", [
+		passport,
+		async function (req, res) {
+			controller.getAds(res, req.query);
+		},
+	]);
 
-  adsRouter.get("/:id", async function (req, res) {
-    const id = req.params.id;
-    controller.getAd(res, id);
-  });
+	adsRouter.get("/:id", [
+		passport,
+		async function (req, res) {
+			const id = req.params.id;
+			controller.getAd(res, id);
+		},
+	]);
 
-  adsRouter.post("/", async function (req, res) {
-    const data = req.body;
-    controller.createAd(res, data);
-  });
+	adsRouter.post("/", async function (req, res) {
+		const data = req.body;
+		controller.createAd(res, data);
+	});
 
-  adsRouter.put("/:id", async function (req, res) {
-    const id = req.params.id;
-    const data = req.body;
-    controller.updateAd(res, id, data);
-  });
+	adsRouter.put("/:id", async function (req, res) {
+		const id = req.params.id;
+		const data = req.body;
+		controller.updateAd(res, id, data);
+	});
 
-  adsRouter.delete("/:id", async function (req, res) {
-    const id = req.params.id;
-    controller.deleteAd(res, id);
-  });
+	adsRouter.delete("/:id", [
+		passportAdmin,
+		async function (req, res) {
+			const id = req.params.id;
+			controller.deleteAd(res, id);
+		},
+	]);
 
-  return adsRouter;
+	return adsRouter;
 })();
