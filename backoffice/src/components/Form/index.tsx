437e3662a9b58459bccbf9051c7a291341db4349
tlugat <teo.lugat@hetic.net>
2022-06-01 09:53:28 +0200

    feat-bo: increase form row gap spacing

--- backoffice/src/components/Form/index.tsx
+++ backoffice/src/components/Form/index.tsx
@@ -4,7 +4,7 @@ export const Form = styled('form')(
     ({ theme }) => `
           display: flex;
           flex-direction: column;
-          row-gap: ${theme.spacing(2)};
+          row-gap: ${theme.spacing(3)};
           width: 100%;
       `
   );
