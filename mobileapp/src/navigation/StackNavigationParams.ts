5d7c8d99ad1d9ffd9420160dee2df00ea41e3343
MaevaWolff <maeva.wolff@hetic.net>
2022-06-03 17:07:18 +0200

    refactor(mobileapp): remove HomeParamList type

--- mobileapp/src/navigation/StackNavigationParams.ts
+++ mobileapp/src/navigation/StackNavigationParams.ts
@@ -3,16 +3,11 @@ import type { ProfileType } from "../screens/uniques/Profile";
 export type StackParamList = {
   Mission: { id: number };
   Profile: { id: number; type: ProfileType };
-} & HomeParamList &
-  AuthParamList &
+} & AuthParamList &
   BottomTabParamList;
 
-export type HomeParamList = {
-  Accueil: undefined;
-  Explications: undefined;
-};
-
 export type AuthParamList = {
+  Explications: undefined;
   "Se connecter": undefined;
   "S'inscrire": undefined;
 };
