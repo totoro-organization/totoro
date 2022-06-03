32131842546f70fcb85e9cf40642854fe9a3080b
MaevaWolff <maeva.wolff@hetic.net>
2022-06-04 01:55:45 +0200

    feat(mobileapp): add user favorites hook and fetch functions

---
+++ mobileapp/src/common/api/requests/getUserFavorites.ts
@@ -0,0 +1,19 @@
+import config from "../config";
+import { API_HOST, API_ROUTES } from "../routes";
+
+export default async function getUserFavorites(userId: string): Promise<any> {
+  const myHeaders = new Headers({
+    "Content-Type": "application/json",
+    app_id: config.APP_ID,
+  });
+
+  const response = await fetch(
+    `${API_HOST}${API_ROUTES.GET_USER_FAVORITES(userId)}`,
+    {
+      method: "GET",
+      headers: myHeaders,
+    }
+  );
+
+  return response;
+}
