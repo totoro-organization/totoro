e431122a4b07dab615905efe32948ff156f81b7c
MaevaWolff <maeva.wolff@hetic.net>
2022-06-04 01:55:45 +0200

    feat(mobileapp): add user favorites hook and fetch functions

--- mobileapp/src/common/api/routes.ts
+++ mobileapp/src/common/api/routes.ts
@@ -5,4 +5,6 @@ export const API_ROUTES = {
   AUTH_LOGIN: "/auth/login", // POST
   AUTH_REGISTER: "/auth/signup", // POST
   AUTH_USER_CONNECTED: "/auth/connected", // GET
+
+  GET_USER_FAVORITES: (userId: string) => `/users/${userId}/favorites`,
 } as const;
---
+++ mobileapp/src/models/favorite.ts
@@ -0,0 +1,8 @@
+export interface Favorite {
+  id: string;
+  user_id: string;
+  assos_id: string;
+  jobs_id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
