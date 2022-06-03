fea470754daed9f59ee1771e8a0c3e64124290b0
MaevaWolff <maeva.wolff@hetic.net>
2022-06-03 21:34:59 +0200

    feat(mobileapp): use bar code scanner

--- mobileapp/src/screens/Scanner.tsx
+++ mobileapp/src/screens/Scanner.tsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GlobalLayout from "../components/layouts/GlobalLayout";
 
+import { BarCodeScanner, PermissionStatus } from "expo-barcode-scanner";
+import Button from "../components/atoms/Button";
+import { Text } from "../components/atoms/Text";
+
 export default function Scanner() {
-  return <GlobalLayout pageTitle="Scanner"></GlobalLayout>;
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    async function getScannerPermissions() {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+
+      setHasPermission(status === PermissionStatus.GRANTED ? true : false);
+    }
+
+    getScannerPermissions();
+  }, []);
+
+  function handleBarCodeScanned({ type, data }: any) {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  }
+
+  return (
+    <GlobalLayout pageTitle="Scanner">
+      {hasPermission === null && (
+        <Text>Dans l'attente de l'autorisation de la caméra.</Text>
+      )}
+      {!hasPermission && <Text>Pas d'accès caméra.</Text>}
+
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      />
+
+      {scanned && (
+        <Button handlePress={() => setScanned(false)}>
+          Scanner un autre code
+        </Button>
+      )}
+    </GlobalLayout>
+  );
 }
