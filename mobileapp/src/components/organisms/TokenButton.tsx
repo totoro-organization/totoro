028b68d89143b2e22323d122741a964dad3a65e3
MaevaWolff <maeva.wolff@hetic.net>
2022-06-04 15:00:54 +0200

    refactor(frontend): add Token button with modal content

---
+++ mobileapp/src/components/organisms/TokenButton.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import Button from "../atoms/Button";
+import Modal from "../atoms/Modal";
+import { Text } from "../atoms/Text";
+import getLocaleCurrencyNotation from "../../common/utils/getLocaleCurrencyNotation";
+import Spacer from "../atoms/Spacer";
+
+/* TODO: Add real user data. */
+const FAKE_DATA_TOKEN = 9328;
+
+export default function TokenButton() {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  return (
+    <View>
+      <Button
+        size="sm"
+        color="primary"
+        variant="outline"
+        handlePress={() => setModalOpen(!modalOpen)}
+      >
+        {getLocaleCurrencyNotation(FAKE_DATA_TOKEN)}
+      </Button>
+
+      <Modal visible={modalOpen} onCloseModal={() => setModalOpen(!modalOpen)}>
+        <Text>Tu as actuellement</Text>
+        <Text size="xl" color="primary" weight="semiBold">
+          {getLocaleCurrencyNotation(FAKE_DATA_TOKEN)} tokens
+        </Text>
+
+        <Spacer axis="vertical" size={1.5} />
+
+        <Text color="grey" align="center">
+          Ces tokens sont gagnés lorsque tu accomplis et valides des missions.
+          Tu peux les dépenser directement dans notre boutique contre des bons
+          de réductions dans les commerces locaux près de chez toi&nbsp;!
+        </Text>
+
+        <Spacer axis="vertical" size={1.5} />
+
+        <View style={{ width: "100%" }}>
+          <Button horizontalPosition="center">Voir la boutique</Button>
+        </View>
+      </Modal>
+    </View>
+  );
+}
