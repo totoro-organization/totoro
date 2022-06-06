995ed8317fa0c0f7527f99c82e73d41133de7696
MaevaWolff <maeva.wolff@hetic.net>
2022-06-06 14:41:57 +0200

    refactor(mobileappp): add spacer element between radios

--- mobileapp/src/components/molecules/RadioGroup.tsx
+++ mobileapp/src/components/molecules/RadioGroup.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { View } from "react-native";
 import Radio from "../atoms/Radio";
+import Spacer from "../atoms/Spacer";
+import { Text } from "../atoms/Text";
 
 type RadioGroupProps = {
   options: string[];
@@ -10,18 +12,23 @@ type RadioGroupProps = {
 export default function RadioGroup({ options, handlePress }: RadioGroupProps) {
   const [radioIndex, setRadioIndex] = useState<number | undefined>(undefined);
 
+  const lastOption = options[options.length - 1];
+
   return (
     <View>
       {options.map((option, index) => (
-        <Radio
-          key={option}
-          option={option}
-          checked={index === radioIndex}
-          handlePress={() => {
-            handlePress(option);
-            setRadioIndex(index);
-          }}
-        />
+        <View key={option}>
+          <Radio
+            option={option}
+            checked={index === radioIndex}
+            handlePress={() => {
+              handlePress(option);
+              setRadioIndex(index);
+            }}
+          />
+
+          {option !== lastOption && <Spacer axis="vertical" size={0.5} />}
+        </View>
       ))}
     </View>
   );
