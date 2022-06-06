15b713361df4fb169da00f6685a838af0772cec3
MaevaWolff <maeva.wolff@hetic.net>
2022-06-06 18:27:43 +0200

    feat(mobileapp): add Sort and Filter svg icons

---
+++ mobileapp/src/assets/icons/Filter.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+import theme from "../../theme/theme";
+
+type FilterProps = {
+  color?: keyof typeof theme.colors;
+  size?: number;
+};
+
+export default function Filter({ color = "grey", size = 16 }: FilterProps) {
+  return (
+    <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
+      <Path
+        d="M22 6.5H16"
+        stroke={theme.colors[color][500]}
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Path
+        d="M6 6.5H2"
+        stroke={theme.colors[color][500]}
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Path
+        d="M10 10C11.933 10 13.5 8.433 13.5 6.5C13.5 4.567 11.933 3 10 3C8.067 3 6.5 4.567 6.5 6.5C6.5 8.433 8.067 10 10 10Z"
+        stroke={theme.colors[color][500]}
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Path
+        d="M22 17.5H18"
+        stroke={theme.colors[color][500]}
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Path
+        d="M8 17.5H2"
+        stroke={theme.colors[color][500]}
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 21C15.933 21 17.5 19.433 17.5 17.5C17.5 15.567 15.933 14 14 14C12.067 14 10.5 15.567 10.5 17.5C10.5 19.433 12.067 21 14 21Z"
+        stroke={theme.colors[color][500]}
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
