fbedcc98da44350617654c15b2b3cf62f5aac7ff
MaevaWolff <maeva.wolff@hetic.net>
2022-06-07 16:55:20 +0200

    refactor(mobileapp): add paginated data interface and fix response

--- mobileapp/src/common/api/hooks/useUserFavorites.ts
+++ mobileapp/src/common/api/hooks/useUserFavorites.ts
@@ -1,12 +1,13 @@
 import { useQuery } from "react-query";
 import { Favorite } from "../../../models/favorite";
+import PaginatedDataType from "../interfaces/PaginatedDataType";
 import getUserFavorites from "../requests/getUserFavorites";
 
 export default function useUserFavorites(userId: string) {
-  const { data, isLoading, error } = useQuery<Favorite[]>(
+  const { data, isLoading, error } = useQuery<PaginatedDataType<Favorite[]>>(
     "getUserFavorites",
     async () => await getUserFavorites(userId)
   );
 
-  return { userFavorites: data, isLoading, error };
+  return { userFavorites: data?.data, isLoading, error };
 }
