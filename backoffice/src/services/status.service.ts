a8a71048fd21b62f1acb0d6cb4875ba205c713ab
tlugat <teo.lugat@hetic.net>
2022-06-08 11:09:34 +0200

    feat-bo: add changeStatus request to status service

--- backoffice/src/services/status.service.ts
+++ backoffice/src/services/status.service.ts
@@ -6,3 +6,8 @@ export async function getStatuses() {
     const response = await requestAxios("GET", STATUS_BASE_URL);
     return response;
 } 
+
+export async function changeStatus(data: object) {
+    const response = await requestAxios("PUT", `/change${STATUS_BASE_URL}`, data);
+    return response;
+}
