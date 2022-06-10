1734f595fd801a9e5a889f0916e95322af44dc76
ElyasCe <im.zoodiac@gmail.com>
2022-06-10 12:33:39 +0200

    feat-bo:user-setting page

--- backoffice/src/pages/User/settings/index.tsx
+++ backoffice/src/pages/User/settings/index.tsx
@@ -5,34 +5,9 @@ import PageTitleWrapper from 'src/components/PageTitleWrapper';
 import { Container, Tabs, Tab, Grid } from '@mui/material';
 import Footer from 'src/components/Footer';
 import { styled } from '@mui/material/styles';
-
-import ActivityTab from './ActivityTab';
 import EditProfileTab from './EditProfileTab';
-import NotificationsTab from './NotificationsTab';
-import SecurityTab from './SecurityTab';
-
-const TabsWrapper = styled(Tabs)(
-  () => `
-    .MuiTabs-scrollableX {
-      overflow-x: auto !important;
-    }
-`
-);
 
 function ManagementUserSettings() {
-
-  const [currentTab, setCurrentTab] = useState<string>('activity');
-
-  const tabs = [
-    { value: 'activity', label: 'Activity' },
-    { value: 'edit_profile', label: 'Edit Profile' },
-    { value: 'notifications', label: 'Notifications' },
-    { value: 'security', label: 'Passwords/Security' }
-  ];
-
-  const handleTabsChange = (event: ChangeEvent<{}>, value: string): void => {
-    setCurrentTab(value);
-  };
 
   return (
     <>
@@ -43,34 +18,9 @@ function ManagementUserSettings() {
         <PageHeader />
       </PageTitleWrapper>
       <Container maxWidth="lg">
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="stretch"
-          spacing={3}
-        >
           <Grid item xs={12}>
-            <TabsWrapper
-              onChange={handleTabsChange}
-              value={currentTab}
-              variant="scrollable"
-              scrollButtons="auto"
-              textColor="primary"
-              indicatorColor="primary"
-            >
-              {tabs.map((tab) => (
-                <Tab key={tab.value} label={tab.label} value={tab.value} />
-              ))}
-            </TabsWrapper>
+            <EditProfileTab />
           </Grid>
-          <Grid item xs={12}>
-            {currentTab === 'activity' && <ActivityTab />}
-            {currentTab === 'edit_profile' && <EditProfileTab />}
-            {currentTab === 'notifications' && <NotificationsTab />}
-            {currentTab === 'security' && <SecurityTab />}
-          </Grid>
-        </Grid>
       </Container>
       <Footer />
     </>
--- backoffice/src/services/config.ts
+++ backoffice/src/services/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    app_id: '18da38de-0626-45a7-a4be-e677cbaf971e',
+    app_id: '4fb3eefe-e007-4fdf-a548-566f045e609b',
     server: process.env['BO_APP_API_BASE_URL']
 }
