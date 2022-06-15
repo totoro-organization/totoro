b7f46379cf482b8958fa34696f0149bd507ac3ab
Pierre-AlexisM <pierre-alexis.maerten@hetic.net>
2022-06-15 23:14:26 +0200

    feat(webapp): rooting mission

---
+++ webapp/src/pages/Gestion/Mission/Liste/indexedDB.tsx
@@ -0,0 +1,20 @@
+import { Helmet } from 'react-helmet-async';
+import PageTitleWrapper from 'src/components/PageTitleWrapper';
+import Footer from 'src/components/Footer';
+
+function CreationMissions() {
+
+    return (
+        <>
+        <Helmet>
+            <title>Manage missions - Applications</title>
+        </Helmet>
+        <PageTitleWrapper>
+        </PageTitleWrapper>
+        <Footer />
+        </>
+    )
+    
+}
+
+export default CreationMissions;
