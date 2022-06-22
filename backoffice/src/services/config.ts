8498c6804b32edc9f66f0b2e0a203b38b6c24ded
tlugat <teo.lugat@hetic.net>
2022-06-22 18:39:16 +0200

    feat-bo: add uri to services config and requestAPi

--- backoffice/src/services/config.ts
+++ backoffice/src/services/config.ts
@@ -1,8 +1,12 @@
-const APP_ID = '74927ee4-0354-49d5-86cc-080e0a4f81d8';
+const APP_ID = '2a61f141-0b84-4e14-860c-963871934e08';
 
 const SERVER = 'http://127.0.0.1:6868';
 
+const URI = '/api'
+
 export const config = {
     app_id: APP_ID,
-    server: SERVER
+    server: SERVER,
+    uri: URI,
+    baseUrl: SERVER + URI
 }
