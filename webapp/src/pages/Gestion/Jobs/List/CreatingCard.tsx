5c39020d2d0f277eb1317303ea3b0ba3f9c0068e
Pierre-AlexisM <pierre-alexis.maerten@hetic.net>
2022-06-23 21:47:20 +0200

    feat: style form creatingJob

--- webapp/src/pages/Gestion/Jobs/List/CreatingCard.tsx
+++ webapp/src/pages/Gestion/Jobs/List/CreatingCard.tsx
@@ -4,13 +4,16 @@ import {
     Card,
     Box,
     Grid,
+    Input,
     Typography,
+    IconButton,
     Hidden,
     Avatar,
     Divider,
     ListItem,
     ListItemText,
     TextField,
+    Container,
     List,
     ListItemAvatar,
     Autocomplete
@@ -23,7 +26,7 @@ import { DatePicker } from '@mui/x-date-pickers';
 function CreatingCard() {
 
     const jobOptions = [
-        'tag1', 'tag 2', 'tag3'
+        'Environnement', 'Culture', 'Communication','Enseignement','Social'
        ];
 
     const [date, setDate] = useState<Date | null>(null);
@@ -43,90 +46,100 @@ function CreatingCard() {
         }) */
 
     return (
-           
-        <Card component="form" noValidate style={{ width: '100%' }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <TextField
-                    required
-                    fullWidth
-                    id="missionType"
-                    label="Type de mission"
-                    name="missionstype"
-                />
-                </Grid>
-                <Grid item xs={12}>
-                    <Autocomplete
-                        limitTags={2}
-                        id="multiple-limit-tags"
-                        options={jobOptions}
-                        renderInput={(params) => (
-                            <TextField {...params} label="Choisissez un tag" placeholder="Favorites" />
-                        )}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <DateFnsProvider>
-                        <DatePicker
-                            label="Basic example"
-                            value={date}
-                            onChange={(newDate) => {
-                            setDate(newDate);
-                            }}
-                            renderInput={(params) => <TextField fullWidth {...params} />}
+        <Container maxWidth="md">
+            <Card component="form">
+                <Box p={4}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                            required
+                            fullWidth
+                            id="missionType"
+                            label="Type de mission"
+                            name="missionstype"
                         />
-                    </DateFnsProvider>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        fullWidth
-                        required
-                        label="Tokens gagnés"
-                        id="outlined-start-adornment"
-                        InputProps={{
-                            endAdornment: <InputAdornment position="end">tokens</InputAdornment>,
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        fullWidth
-                        required
-                        label="Nombre de places"
-                        id="outlined-start-adornment"
-                        InputProps={{
-                            endAdornment: <InputAdornment position="end">places</InputAdornment>,
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        required
-                        multiline
-                        rows={4}
-                        fullWidth
-                        id="missionDescription"
-                        label="Description"
-                        name="missiondescription"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        name="upload-photo"
-                        type="file"
-                    />
-                </Grid>
-                <Button
-                    disabled={false}
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    >
-                    Créer mission
-                </Button>
-            </Grid>
-        </Card>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Autocomplete
+                                limitTags={2}
+                                id="multiple-limit-tags"
+                                options={jobOptions}
+                                renderInput={(params) => (
+                                    <TextField {...params} label="Choisissez un tag" placeholder="Favorites" />
+                                )}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <DateFnsProvider>
+                                <DatePicker
+                                    label="Date de commencement"
+                                    value={date}
+                                    onChange={(newDate) => {
+                                    setDate(newDate);
+                                    }}
+                                    renderInput={(params) => <TextField fullWidth {...params} />}
+                                />
+                            </DateFnsProvider>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                required
+                                inputProps={{ min: 0 }}
+                                label="Tokens gagnés"
+                                type='number'
+                                id="outlined-start-adornment"
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">tokens</InputAdornment>,
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                fullWidth
+                                required
+                                inputProps={{ min: 0 }}
+                                type='number'
+                                label="Nombre de places"
+                                id="outlined-start-adornment"
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">places</InputAdornment>,
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                multiline
+                                rows={4}
+                                fullWidth
+                                id="missionDescription"
+                                label="Description"
+                                name="missiondescription"
+                            />
+                        </Grid>
+                        <label htmlFor="contained-button-file">
+                            <Input style={{display:'none'}} id="contained-button-file" type="file" />
+                            <Button variant="contained" component="span">
+                                Upload
+                            </Button>
+                        </label>
+                        {/* <TextField
+                            name="upload-photo"
+                            type="file"
+                        /> */}
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            >
+                            Create your job
+                        </Button>
+                    </Grid>
+                </Box>
+            </Card>
+        </Container>
     )
         
   }
