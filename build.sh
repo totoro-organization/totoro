bad508d2ab7330d9c808e11a99f4a1a8b5cefcfb
tlugat <teo.lugat@hetic.net>
2022-06-27 19:27:38 +0200

    feat-infrastructure: cf last commit

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ do
             ;;
         --projects) 
             req="docker-compose up db phpmyadmin api"
-            IFS=";" read -a images <<< "$val";
+            IFS=";" read -a images <<< "$val"
             for i in "${images[@]}"
             do
                 req+=" $i"
