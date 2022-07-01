91305d18e18d403f1e10611124d07d9370950e45
MaevaWolff <maeva.wolff@hetic.net>
2022-07-01 17:16:21 +0200

    feat(webapp): add unit test for getStringToColor function

--- webapp/src/utils/getStringToColor.ts
+++ webapp/src/utils/getStringToColor.ts
@@ -1,9 +1,9 @@
-export function getStringToColor(string: string) {
+export function getStringToColor(value: string): string {
   let hash = 0;
   let i;
 
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  for (i = 0; i < value.length; i += 1) {
+    hash = value.charCodeAt(i) + ((hash << 5) - hash);
   }
 
   let color = '#';
