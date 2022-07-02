1733b6ec4819852fe1ce9eed9206928250af9e7d
MaevaWolff <maeva.wolff@hetic.net>
2022-07-02 21:37:41 +0200

    feat(mobileapp): add conversation not found state to messaging screen

---
+++ mobileapp/src/screens/Chat.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "../components/atoms/Text";
+import MainLayout from "../components/layouts/MainLayout";
+
+export default function Chat() {
+  return (
+    <MainLayout>
+      <Text>Chat</Text>
+    </MainLayout>
+  );
+}
