47ff1ea336f1e0412f140fdf47783a72e98d3263
MaevaWolff <maeva.wolff@hetic.net>
2022-07-02 14:28:59 +0200

    fix(mobileapp): add type of useJobs result

--- mobileapp/src/common/api/hooks/useJobs.ts
+++ mobileapp/src/common/api/hooks/useJobs.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "react-query";
+import type { Job } from "../../../models/job";
 import PaginatedDataType from "../interfaces/PaginatedDataType";
 import getJobs from "../requests/getJobs";
 
 export default function useJobs() {
-  const { data, isLoading, error } = useQuery<PaginatedDataType<any>>(
+  const { data, isLoading, error } = useQuery<PaginatedDataType<Job>>(
     "getJobs",
     async () => await getJobs()
   );
