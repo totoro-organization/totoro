f9b2dba22a2337aad98f23485c981302b8b8208c
MaevaWolff <maeva.wolff@hetic.net>
2022-07-02 21:37:41 +0200

    feat(mobileapp): add conversation not found state to messaging screen

--- mobileapp/src/screens/Messaging.tsx
+++ mobileapp/src/screens/Messaging.tsx
@@ -1,19 +1,15 @@
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
 import React from "react";
-import { TouchableOpacity } from "react-native";
 import { FAKE_MESSAGES_DATA } from "../common/mockedData";
 import Spacer from "../components/atoms/Spacer";
-import { Text } from "../components/atoms/Text";
 import GlobalLayout from "../components/layouts/GlobalLayout";
 import ConversationItem from "../components/molecules/Conversation/ConversationItem";
-import { StackParamList } from "../navigation/StackNavigationParams";
+import ConversationsNotFound from "../components/molecules/Conversation/ConversationsNotFound";
 
 export default function Messaging() {
-  const navigation = useNavigation<StackNavigationProp<StackParamList>>();
-
   return (
     <GlobalLayout>
+      {FAKE_MESSAGES_DATA.length < 0 && <ConversationsNotFound />}
+
       {/* TODO: Replace me with real data (hooks) */}
       {FAKE_MESSAGES_DATA.map((conversation, index) => (
         <>
