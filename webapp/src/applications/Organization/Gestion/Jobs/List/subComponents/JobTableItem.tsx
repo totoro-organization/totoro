5c2134a780757808cb540595e89139da06280567
MaevaWolff <maeva.wolff@hetic.net>
2022-07-02 05:12:25 +0200

    fix(webapp): improve readability of JobTableItem

--- webapp/src/applications/Organization/Gestion/Jobs/List/subComponents/JobTableItem.tsx
+++ webapp/src/applications/Organization/Gestion/Jobs/List/subComponents/JobTableItem.tsx
@@ -42,6 +42,7 @@ export default function JobTableItem({
           value={isSelectedJob}
         />
       </TableCell>
+
       <TableCell>
         <Typography
           variant="body1"
@@ -52,12 +53,14 @@ export default function JobTableItem({
         >
           <Link to={`/gestion/missions/${job.id}`}>{job.title}</Link>
         </Typography>
+
         <Typography variant="body2" color="text.secondary" noWrap>
           {job.author.organization
             ? job.author.organization.name
             : `${job.author.user.firstname} ${job.author.user.lastname} (${job.author.user.username})`}
         </Typography>
       </TableCell>
+
       <TableCell>
         <Typography
           variant="body1"
@@ -69,6 +72,7 @@ export default function JobTableItem({
           {format(new Date(job.createdAt), 'dd/MM/yyyy HH:mm:ss')}
         </Typography>
       </TableCell>
+
       <TableCell>
         <Typography
           variant="body1"
@@ -80,6 +84,7 @@ export default function JobTableItem({
           {job.participants_max - job.remaining_place} / {job.participants_max}
         </Typography>
       </TableCell>
+
       <TableCell align="right">
         <Typography
           variant="body1"
@@ -91,9 +96,11 @@ export default function JobTableItem({
           {job.difficulty.token}
         </Typography>
       </TableCell>
+
       <TableCell align="right">
         <StatusLabel status={job.status.label} />
       </TableCell>
+
       <TableCell align="right">
         <Tooltip title="Supprimer la mission" arrow>
           <IconButton
