eb433e3bbba2e809682d56c2af5e129496e79f74
MaevaWolff <maeva.wolff@hetic.net>
2022-07-02 16:00:27 +0200

    feat(mobileapp): add role, partner, discount, user data interfaces

---
+++ mobileapp/src/models/role.ts
@@ -0,0 +1,12 @@
+export interface Role {
+  id: string;
+  label: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export enum RoleEnum {
+  admin = "Administrateur",
+  moderator = "Moderateur",
+  accountant = "Comptables",
+}
