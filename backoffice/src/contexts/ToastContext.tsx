754b1e5642d8f65f288398d7ab82acd7d40c2a69
tlugat <teo.lugat@hetic.net>
2022-07-02 14:07:26 +0200

    feat-bo: add toast context and toast component

---
+++ backoffice/src/contexts/ToastContext.tsx
@@ -0,0 +1,56 @@
+
+import { styled } from "@mui/styles";
+import { Box } from "@mui/system";
+import { useCallback, useContext, useState, createContext, SyntheticEvent } from "react";
+import ToastComponent from 'src/components/Toast';
+
+export interface Toast {
+    variant: "error" | "warning" | "info" | "success",
+    text: string,
+    duration?: number
+}
+
+interface ToastContext {
+    toasts: Toast[]
+    addToast: (toast: Toast) => void
+}
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+const ToastContext = createContext<any>(function(){} as ToastContext["addToast"]);
+
+const ToastWrapper = styled(Box)({
+  position: 'fixed',
+  bottom: '2rem',
+  right: '2rem',
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: '1rem'
+})
+
+export function ToastProvider({ children }) {
+  const [toasts, setToasts] = useState<Toast[]>([]);
+    
+    const addToast = useCallback(
+        function (toast: Toast) {
+            setToasts((toasts) => [...toasts, toast]);
+            setTimeout(() => setToasts((toasts) => toasts.slice(1)), toast.duration ?? 3000);
+        },
+        [setToasts]
+    );
+
+  return (
+    <ToastContext.Provider value={{addToast}}>
+      {children}
+      <ToastWrapper>
+        {toasts.map((toast, i) => (
+            <ToastComponent key={i} variant={toast.variant} text={toast.text}/>
+        ))}
+      </ToastWrapper>
+    </ToastContext.Provider>
+  );
+}
+
+export function useToast() {
+  return useContext(ToastContext);
+}
+
+export default ToastContext;
