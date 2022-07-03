a4b12d7ea9faa39e45fcdb34da2e9da89a6ed0cf
MaevaWolff <maeva.wolff@hetic.net>
2022-07-03 17:29:38 +0200

    feat(mobileapp): add routes file to cypress folder

---
+++ mobileapp/cypress/support/routes.ts
@@ -0,0 +1,7 @@
+// TODO: Set this base url with Cypress.env()
+export const FRONTEND_BASE_URL = "http://localhost:19006";
+export const API_BASE_URL = "http://127.0.0.1:6868/api";
+
+export const API_ROUTES = {
+  LOGIN: `${API_BASE_URL}/auth/login`,
+} as const;
