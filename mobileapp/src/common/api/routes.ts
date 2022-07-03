13f558c908624ea69ea44925d00ab57f5f80e4bc
MaevaWolff <maeva.wolff@hetic.net>
2022-07-03 22:49:38 +0200

    refactor(mobileapp): improve addUserFavorite function

--- mobileapp/src/common/api/routes.ts
+++ mobileapp/src/common/api/routes.ts
@@ -7,7 +7,7 @@ export const API_ROUTES = {
   AUTH_REGISTER: "/auth/signup", // POST
   AUTH_USER_CONNECTED: "/auth/connected", // GET
 
-  USER_FAVORITES: (userId: string) => `/users/${userId}/favorites`, // GET, POST
+  USER_FAVORITES: (itemId: string) => `/users/${itemId}/favorites`, // GET, POST
   USER_JOBS: (userId: string) => `/users/${userId}/jobs`, // GET
 
   FAVORITES: (favoriteId: string) => `/favorites/${favoriteId}`, // DELETE
