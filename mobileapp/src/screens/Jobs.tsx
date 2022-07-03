59db8c88d84d7941bdc4657620c18b68be2c61e6
MaevaWolff <maeva.wolff@hetic.net>
2022-07-03 19:58:58 +0200

    refactor(mobileapp): add loading screen on jobs

--- mobileapp/src/screens/Jobs.tsx
+++ mobileapp/src/screens/Jobs.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import React, { useState } from "react";
+import { ActivityIndicator } from "react-native";
 import Filter from "../assets/icons/Filter";
 import Sort from "../assets/icons/Sort";
 import useJobs from "../common/api/hooks/useJobs";
@@ -9,11 +10,13 @@ import Box from "../components/atoms/Box";
 import Button from "../components/atoms/Button";
 import Modal from "../components/atoms/Modal";
 import Spacer from "../components/atoms/Spacer";
+import Spinner from "../components/atoms/Spinner";
 import { Text } from "../components/atoms/Text";
 
 import GlobalLayout from "../components/layouts/GlobalLayout";
 import JobsList from "../components/organisms/JobsList";
 import { AppParamList } from "../navigation/StackNavigationParams";
+import theme from "../theme/theme";
 
 export default function Jobs() {
   const navigation = useNavigation<StackNavigationProp<AppParamList>>();
@@ -54,8 +57,9 @@ export default function Jobs() {
 
       <Spacer axis="vertical" size={2} />
 
-      {/* TODO!!!! */}
-      {loadingJobs && <Text>Add loading screen</Text>}
+      {loadingJobs && (
+        <ActivityIndicator color={theme.colors.brand.primary.base} />
+      )}
       {!loadingJobs && <JobsList data={jobs} />}
     </GlobalLayout>
   );
