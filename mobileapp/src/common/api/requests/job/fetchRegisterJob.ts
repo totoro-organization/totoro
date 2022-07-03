136aab537e90a4a5c211f9790679394ad0e226b4
MaevaWolff <maeva.wolff@hetic.net>
2022-07-03 22:52:50 +0200

    refactor(mobileapp): improve fetchRegisterJob function

--- mobileapp/src/common/api/requests/job/fetchRegisterJob.ts
+++ mobileapp/src/common/api/requests/job/fetchRegisterJob.ts
@@ -2,8 +2,12 @@ import config from "../../config";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { API_HOST, API_ROUTES } from "../../routes";
+import Toast from "react-native-toast-message";
 
-export default async function fetchRegisterJob(job_id: string): Promise<any> {
+export default async function fetchRegisterJob(
+  job_id: string,
+  jobName: string
+): Promise<any> {
   const userToken = await AsyncStorage.getItem("userToken");
   const bearer = "Bearer" + " " + userToken;
 
@@ -13,8 +17,25 @@ export default async function fetchRegisterJob(job_id: string): Promise<any> {
     Authorization: bearer,
   });
 
-  return fetch(`${API_HOST}${API_ROUTES.JOB_REGISTER(job_id)}`, {
-    method: "POST",
-    headers: myHeaders,
-  });
+  try {
+    const response = await fetch(
+      `${API_HOST}${API_ROUTES.JOB_REGISTER(job_id)}`,
+      {
+        method: "POST",
+        headers: myHeaders,
+      }
+    );
+
+    if (response.status === 201) {
+      Toast.show({
+        type: "success",
+        props: {
+          title: "Tout est bon",
+          text: `Tu t'es bien inscrit à la mission ${jobName} !`,
+        },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
