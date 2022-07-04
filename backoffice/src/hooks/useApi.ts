354c4bc71f0c9a854c04cbc1d01dfb4c928aac46
tlugat <teo.lugat@hetic.net>
2022-07-04 13:13:51 +0200

    feat-bo: handle error

--- backoffice/src/hooks/useApi.ts
+++ backoffice/src/hooks/useApi.ts
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { getItems } from 'src/services/common.service';
 import { Route } from 'src/services/routes';
+import { useToast } from './useToast';
 
 export type ApiResponse = {
   data: any;
   error: any;
   loading: boolean;
+  getData: () => Promise<any>
 };
 
 export const useApi = (url: Route, query?: any): ApiResponse => {
@@ -13,20 +15,31 @@ export const useApi = (url: Route, query?: any): ApiResponse => {
   const [error, setError] = useState<any>();
   const [loading, setLoading] = useState<boolean>(false);
 
-  const getAPIData = async () => {
+  const { setToast } = useToast();
+
+  const getAPIData = async (): Promise<any> => {
+    if(error) setError(null);
     setLoading(true);
     try {
-      const apiResponse = await getItems(url, query);
-      setData(apiResponse);
+      const response = await getItems(url, query);
+      if('error' in response) {
+        setError(response.message);
+        setToast({ variant: 'error', message: response.message, duration: 6000})
+        return;
+      }
+      setData(response);
     } catch (error) {
       setError(error);
+      console.log('test');
     }
     setLoading(false);
   };
 
   useEffect(() => {
-    getAPIData();
+    if(!error) {
+      getAPIData();
+    }
   }, []);
 
-  return { data, error, loading };
+  return { data, error, loading, getData: getAPIData };
 };
