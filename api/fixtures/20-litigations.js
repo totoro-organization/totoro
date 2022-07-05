f19aa810dfdc80c322e1b483e13999d7c14711db
j2frise <jarce2frise@yahoo.fr>
2022-07-05 09:33:16 +0200

    test

--- api/fixtures/20-litigations.js
+++ api/fixtures/20-litigations.js
@@ -5,7 +5,7 @@ module.exports = [
       litigation_object: {
         label: "Spam",
       },
-      type: 0,
+      type: false,
       message: "L'utilisateur envoie des publicités de manière répétée.",
       mission: {
         id: "f7f5bbf1-5af6-402e-a5ac-d3ec4cd6494g",
