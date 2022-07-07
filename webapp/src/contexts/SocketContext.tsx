4afc0e301599a27bddb46f5df568dd65b3dc9d6b
tlugat <teo.lugat@hetic.net>
2022-07-07 19:45:58 +0200

    feat-webapp: add socket context and custom hook

---
+++ webapp/src/contexts/SocketContext.tsx
@@ -0,0 +1,13 @@
+
+import { ReactJSXElementChildrenAttribute } from "@emotion/react/types/jsx-namespace";
+import { createContext } from "react";
+
+const ws = new WebSocket('');
+
+const SocketContext = createContext(ws);
+
+export function SocketProvider({ children }: ReactJSXElementChildrenAttribute) {
+  return <SocketContext.Provider value={ws}>{children}</SocketContext.Provider>
+}
+
+export default SocketContext;
