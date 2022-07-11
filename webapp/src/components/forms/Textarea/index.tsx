f0c65ebb5e1a83e545629851b9596c98ec371a67
tlugat <teo.lugat@hetic.net>
2022-07-11 13:00:03 +0200

    refacto-webapp: use useForm in TextArea

--- webapp/src/components/forms/Textarea/index.tsx
+++ webapp/src/components/forms/Textarea/index.tsx
@@ -1,14 +1,23 @@
 import { FormControl, InputLabel, TextareaAutosize } from '@mui/material';
 import { Controller } from 'react-hook-form';
 
+interface IFormTextArea {
+  name: string;
+  label?: string;
+  defaultValue?: string;
+  minRows?: number;
+  placeholder?: string;
+}
+
 const FormTextarea = ({
   name,
   minRows = 3,
+  defaultValue,
   placeholder = null,
   ...props
-}) => {
+}: IFormTextArea) => {
   return (
-      <Controller
+    <Controller
       name={name}
       render={({ field, fieldState: { error } }) => (
         <TextareaAutosize
@@ -16,10 +25,10 @@ const FormTextarea = ({
           aria-label="minimum height"
           minRows={minRows}
           placeholder={placeholder}
-          style={{ width: "100%", height: 150 ,maxWidth: ""}}
+          style={{ width: '100%', height: 150, maxWidth: '' }}
         />
       )}
-      {...props}
+      defaultValue={defaultValue}
     />
   );
 };
