c297a8238a5e4e4e9dc1c167b93f3ec2b763c6d9
MaevaWolff <maeva.wolff@hetic.net>
2022-07-11 17:35:53 +0200

    fix(mobileapp): update api base url to cypress

--- mobileapp/cypress/support/routes.ts
+++ mobileapp/cypress/support/routes.ts
@@ -1,6 +1,6 @@
 // TODO: Set this base url with Cypress.env()
 export const FRONTEND_BASE_URL = "http://localhost:19006/";
-export const API_BASE_URL = "http://127.0.0.1:6868/api";
+export const API_BASE_URL = "https://api-totoro.herokuapp.com/";
 
 export const API_ROUTES = {
   LOGIN: `${API_BASE_URL}/auth/login`,
