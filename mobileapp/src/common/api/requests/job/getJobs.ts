f3f7b01a7b0a4a58f68282865ff4b08c5fc756c5
MaevaWolff <maeva.wolff@hetic.net>
2022-07-11 18:42:02 +0200

    fix(mobileapp): fix request jobs parameters

--- mobileapp/src/common/api/requests/job/getJobs.ts
+++ mobileapp/src/common/api/requests/job/getJobs.ts
@@ -3,10 +3,15 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { API_HOST, API_ROUTES } from "../../routes";
 
-export default async function getJobs(parameters: {
-  longitude: number;
-  latitude: number;
-}): Promise<any> {
+export interface JobsRequestParameters {
+  longitude?: number;
+  latitude?: number;
+}
+
+export default async function getJobs({
+  longitude,
+  latitude,
+}: JobsRequestParameters): Promise<any> {
   const userToken = await AsyncStorage.getItem("userToken");
   const bearer = "Bearer" + " " + userToken;
 
@@ -17,7 +22,7 @@ export default async function getJobs(parameters: {
   });
 
   const response = await fetch(
-    `${API_HOST}${API_ROUTES.JOBS}?longitude=${parameters.longitude}&latitude=${parameters.latitude}`,
+    `${API_HOST}${API_ROUTES.JOBS}?longitude=${longitude}&latitude=${latitude}`,
     {
       method: "GET",
       headers: myHeaders,
