de8374378c23bc4f83b734fa8654eec2997e3a6c
tlugat <teo.lugat@hetic.net>
2022-07-12 21:24:51 +0200

    refacto-webapp: rename job form schema file

--- webapp/src/components/JobForm/JobForm.schema.ts
+++ webapp/src/components/JobForm/JobForm.schema.ts
@@ -1,17 +1,14 @@
 import * as Yup from "yup";
-import { EMAIL_VALID_MSG, REQUIRED_MSG } from "../forms/validationConstants";
-
-export type JobFormFieldTypes = Yup.InferType<
-  typeof JobFormSchema
->;
+import { REQUIRED_MSG } from "../forms/validationConstants";
 
 export const JobFormSchema = Yup.object({
   title: Yup.string().min(3).required(REQUIRED_MSG).trim(),
-  tags: Yup.array().of(Yup.object()).min(1).required(REQUIRED_MSG),
+  tags: Yup.array().of(Yup.object()).min(1),
   category: Yup.object().required(REQUIRED_MSG),
   difficulty: Yup.string().required(REQUIRED_MSG),
   nb_participants: Yup.number().min(1),
-  description: Yup.string().min(100).required(REQUIRED_MSG),
+  description: Yup.string().min(50).required(REQUIRED_MSG),
   start_date: Yup.date().required(REQUIRED_MSG),
-  end_date: Yup.date().required(REQUIRED_MSG)
+  end_date: Yup.date().required(REQUIRED_MSG),
+  images: Yup.array().of(Yup.string()).min(1)
 }).required(REQUIRED_MSG);
