effea8defef7fc4ea4030bbbb1c30b697b9fe199
tlugat <teo.lugat@hetic.net>
2022-07-12 21:26:52 +0200

    refacto-webapp: update inputs

--- webapp/src/components/JobForm/index.tsx
+++ webapp/src/components/JobForm/index.tsx
@@ -1,65 +1,90 @@
-// @ts-noCheck
 import { Button, MenuItem } from '@mui/material';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { FormContainer, FormSelect, FormTextarea } from 'src/components/forms';
-import { JobFormFieldTypes, JobFormSchema } from './JobFormSchema';
+import { FormContainer, FormInputFile, FormSelect, FormTextarea } from 'src/components/forms';
+import { JobFormSchema } from './JobForm.schema';
 import {
   FormAutocomplete,
   FormTextField,
-  FormDatePickerDuo,
+  FormDateTimeRangePicker
 } from 'src/components/forms';
 import { useContext } from 'react';
 import { CommonsContext } from 'src/contexts/CommonsContext';
 import { sortObjectArrayByAscOrder } from 'src/utils/sortByAscOrder';
 import type { JobDifficulty, Tag } from 'src/models';
+import format from 'date-fns/format';
 
-// interface IFormDatePickerDuoFieldsTypes {
-//   start_date: JobFormFieldTypes["start_date"],
-//   end_date: JobFormFieldTypes["end_date"]
-// }
+interface JobFormFieldTypes {
+  start_date: Date
+  end_date: Date
+  nb_participants: number,
+  description: string,
+  tags: Tag[],
+  category: Tag,
+  difficulty: JobDifficulty["id"],
+  title: string,
+  images: string[]
+}
+
+interface JobFormData {
+  start_date: string,
+  end_date: string,
+  nb_participants: number,
+  description: string,
+  tags: Tag["id"][],
+  difficulty: JobDifficulty['id'],
+  title: string,
+  images: string[]
+}
 
 const JobForm = () => {
-  const {
-    handleSubmit,
-    reset,
-    control,
-    formState: { errors }
-  } = useForm<JobFormFieldTypes>({ resolver: yupResolver(JobFormSchema) });
+  
+  const methods = useForm<JobFormFieldTypes>({ resolver: yupResolver(JobFormSchema) });
   const { tags, difficulties, categories } = useContext(CommonsContext);
 
-  const onSubmit = (data: JobFormFieldTypes) => console.log(data);
+  const onSubmit = (formData: JobFormFieldTypes) => {
+    const tagsId = formData.tags.map((tag: Tag) => tag.id);
+    const data: JobFormData = {
+      start_date: format(formData.start_date, 'yyyy-MM-dd HH:mm'),
+      end_date: format(formData.end_date, 'yyyy-MM-dd HH:mm'),
+      tags: [...tagsId, formData.category.id],
+      title: formData.title,
+      nb_participants: formData.nb_participants,
+      description: formData.description,
+      difficulty: formData.difficulty,
+      images: formData.images
+    }
+    console.log(data)
+  };
+  
   const ascDifficulties: JobDifficulty[] = sortObjectArrayByAscOrder(
     difficulties,
     'level'
   );
 
   return (
-    <FormContainer>
-      <FormTextField
-        defaultValue=""
-        control={control}
-        label="Titre"
-        name="title"
-      />
-      <FormAutocomplete
+    <FormProvider {...methods}>
+      <FormContainer onSubmit={methods.handleSubmit(onSubmit)}>
+        <FormTextField defaultValue="" label="Titre" name="title" />
+        <FormAutocomplete
         multiple
         options={tags}
-        control={control}
         label="Tags"
         name="tags"
+        getOptionLabel={(option) => option.label}
+        defaultValue={[]}
       />
-      <FormAutocomplete
+        <FormAutocomplete
         options={categories}
-        control={control}
         label="Catégories"
         name="category"
+        getOptionLabel={(option) => option.label}
+        defaultValue={null}
       />
-      <FormSelect<JobFormFieldTypes>
-        defaultValue={ascDifficulties[0].id}
-        control={control}
+      <FormSelect
         label="Difficulté"
         name="difficulty"
+        defaultValue={ascDifficulties[0] ?? ""}
       >
         {ascDifficulties.map((difficulty: JobDifficulty) => (
           <MenuItem key={difficulty.id} value={difficulty.id}>
@@ -67,25 +92,25 @@ const JobForm = () => {
           </MenuItem>
         ))}
       </FormSelect>
-      <FormDatePickerDuo<JobFormFieldTypes> control={control} />
-      <FormTextField
-        inputProps={{ min: 0 }}
-        type="number"
-        defaultValue={0}
-        control={control}
-        label="Nombre de participants"
-        name="nb_participants"
-      />
-      <FormTextarea
-        placeholder="Description"
-        control={control}
-        name="description"
-      />
-      <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
-      <Button onClick={() => reset()} variant={'outlined'}>
-        Reset
-      </Button>
-    </FormContainer>
+        <FormDateTimeRangePicker name="date"/>
+        <FormTextField
+          inputProps={{ min: 0 }}
+          type="number"
+          defaultValue={0}
+          label="Nombre de participants"
+          name="nb_participants"
+        />
+        <FormTextarea
+          placeholder="Description"
+          name="description"
+        />
+        <FormInputFile/>
+        <Button type="submit">Submit</Button>
+        <Button onClick={() => methods.reset()} variant={'outlined'}>
+          Reset
+        </Button>
+      </FormContainer>
+    </FormProvider>
   );
 };
 
