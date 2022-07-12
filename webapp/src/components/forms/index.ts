326244a280a37543990e8b5ffb1e954b1f2b63cc
tlugat <teo.lugat@hetic.net>
2022-07-12 19:04:48 +0200

    refacto-webapp: update DateTimeRangePicker import

--- webapp/src/components/forms/index.ts
+++ webapp/src/components/forms/index.ts
@@ -4,14 +4,14 @@ import FormContainer from "./FormContainer";
 import FormSelect from "./Select";
 import FormTextarea from "./Textarea";
 import FormDateTimePicker from "./DateTimePicker";
-import FormDatePickerDuo from "./DateTimePickerDuo";
+import FormDateTimeRangePicker from "./DateTimeRangePicker";
 
 export {
     FormTextField,
     FormAutocomplete,
     FormContainer,
     FormSelect,
-    FormDatePickerDuo,
+    FormDateTimeRangePicker,
     FormDateTimePicker,
     FormTextarea
 }
