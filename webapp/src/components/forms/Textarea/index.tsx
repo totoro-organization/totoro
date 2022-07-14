b702a436cd49ccb54856c971221e46f95d38d879
tlugat <teo.lugat@hetic.net>
2022-07-14 16:55:41 +0200

    feat-webapp: display erro in TextArea

--- webapp/src/components/forms/Textarea/index.tsx
+++ webapp/src/components/forms/Textarea/index.tsx
@@ -1,5 +1,6 @@
-import { TextareaAutosize, TextareaAutosizeProps } from '@mui/material';
+import { FormControl, FormHelperText, TextareaAutosize, TextareaAutosizeProps } from '@mui/material';
 import { Controller, useFormContext } from 'react-hook-form';
+
 
 const FormTextarea = ({
   name,
@@ -13,13 +14,17 @@ const FormTextarea = ({
       name={name}
       control={control}
       render={({ field, fieldState: { error } }) => (
-        <TextareaAutosize
+        <FormControl>
+          <TextareaAutosize
           {...field}
           aria-label="minimum height"
           style={{ width: '100%', height: 150, maxWidth: '' }}
           onChange={(e) => console.log(e)}
           {...props}
         />
+        {!!error && <FormHelperText error={!!error}>{error.message}</FormHelperText>}
+        </FormControl>
+        
       )}
       defaultValue={defaultValue}
     />
