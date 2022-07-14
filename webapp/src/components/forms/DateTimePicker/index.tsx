edc7a0a2dbe25efeec4637632b873223cceee784
tlugat <teo.lugat@hetic.net>
2022-07-14 16:55:17 +0200

    feat-webapp: display error in date time picker and add default value to end_date (dateRangePicker)

--- webapp/src/components/forms/DateTimePicker/index.tsx
+++ webapp/src/components/forms/DateTimePicker/index.tsx
@@ -1,24 +1,24 @@
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { FormControl, TextField } from '@mui/material';
+import { FormControl, FormHelperText, TextField } from '@mui/material';
 import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { format } from 'date-fns';
 import { useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
 interface IFormDateTimePicker {
-  name: string
-  label?: string,
-  minDateTime?: any,
-  maxDateTime?: any,
-  handleChangeDate?: (date: Date) => void,
-  defaultValue?: any,
-  disabled?: boolean,
-  minutesStep?: number,
-  disablePast?: boolean,
-  minDate?: any,
-  maxDate?: any,
-  minTime?: any,
-  maxTime?: any
+  name: string;
+  label?: string;
+  minDateTime?: any;
+  maxDateTime?: any;
+  handleChangeDate?: (date: Date) => void;
+  defaultValue?: any;
+  disabled?: boolean;
+  minutesStep?: number;
+  disablePast?: boolean;
+  minDate?: any;
+  maxDate?: any;
+  minTime?: any;
+  maxTime?: any;
 }
 
 function FormDateTimePicker({
@@ -36,16 +36,15 @@ function FormDateTimePicker({
   disablePast,
   minutesStep = 1,
   ...props
-}: IFormDateTimePicker) : JSX.Element{
-
+}: IFormDateTimePicker): JSX.Element {
   const { control } = useFormContext();
 
   const [value, setValue] = useState<Date | string>(defaultValue ?? null);
 
   const handleChange = (date, onChange) => {
-    setValue(date);        
+    setValue(date);
     onChange(date);
-    if(handleChangeDate) handleChangeDate(date);
+    if (handleChangeDate) handleChangeDate(date);
   };
 
   return (
@@ -55,23 +54,28 @@ function FormDateTimePicker({
           control={control}
           name={name}
           render={({ field, fieldState: { error } }) => (
-            <DateTimePicker
-              disablePast={disablePast}
-              minutesStep={minutesStep}
-              disabled={disabled}
-              minDateTime={minDateTime}
-              maxDateTime={maxDateTime}
-              minTime={minTime}
-              maxTime={maxTime}
-              minDate={minDate}
-              maxDate={maxDate}
-              inputFormat="dd/MM/yyyy HH:mm"
-              label={label}
-              value={value}
-              onChange={(data) => handleChange(data, field.onChange)}
-              renderInput={(params) => <TextField {...params} />}
-              onError={(error, value) => console.log(error, value)}
-            />
+            <FormControl>
+              <DateTimePicker
+                disablePast={disablePast}
+                minutesStep={minutesStep}
+                disabled={disabled}
+                minDateTime={minDateTime}
+                maxDateTime={maxDateTime}
+                minTime={minTime}
+                maxTime={maxTime}
+                minDate={minDate}
+                maxDate={maxDate}
+                inputFormat="dd/MM/yyyy HH:mm"
+                label={label}
+                value={value}
+                onChange={(data) => handleChange(data, field.onChange)}
+                renderInput={(params) => <TextField {...params} />}
+                onError={(error, value) => console.log(error, value)}
+              />
+              {!!error && (
+                <FormHelperText error={!!error}>{error.message}</FormHelperText>
+              )}
+            </FormControl>
           )}
           defaultValue={defaultValue}
         />
