cf2ffa08b15df6c47b1229a5cb6c4b43afe083cb
tlugat <teo.lugat@hetic.net>
2022-07-14 18:01:07 +0200

    fix-webapp: fix date time range picker

--- webapp/src/components/JobForm/JobForm.schema.ts
+++ webapp/src/components/JobForm/JobForm.schema.ts
@@ -1,5 +1,5 @@
-import * as Yup from "yup";
-import { REQUIRED_MSG } from "../forms/validationConstants";
+import * as Yup from 'yup';
+import { REQUIRED_MSG } from '../forms/validationConstants';
 
 export const JobFormSchema = Yup.object({
   title: Yup.string().min(3).required(REQUIRED_MSG).trim(),
@@ -8,7 +8,7 @@ export const JobFormSchema = Yup.object({
   difficulty: Yup.string().required(REQUIRED_MSG),
   nb_participants: Yup.number().min(1),
   description: Yup.string().min(50).required(REQUIRED_MSG),
-  start_date: Yup.date().required(REQUIRED_MSG),
-  end_date: Yup.date().required(REQUIRED_MSG),
+  start_date: Yup.string().required(REQUIRED_MSG),
+  end_date: Yup.string().required(REQUIRED_MSG),
   images: Yup.mixed().required(REQUIRED_MSG)
 }).required(REQUIRED_MSG);
