255fc3ca567924be4add57365e0c8bf5f22c913d
MaevaWolff <maeva.wolff@hetic.net>
2022-07-16 17:49:15 +0200

    refactor(mobileapp): check discounts loading state to Shop page

--- mobileapp/src/screens/Shop.tsx
+++ mobileapp/src/screens/Shop.tsx
@@ -3,25 +3,32 @@ import GlobalLayout from "../components/layouts/GlobalLayout";
 import ShopCard from "../components/molecules/ShopCard";
 import styled from "styled-components/native";
 import Spacer from "../components/atoms/Spacer";
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import useDiscounts from "../common/api/hooks/useDiscounts";
+import theme from "../theme/theme";
 
 export default function Shop() {
-  const { discounts } = useDiscounts();
+  const { discounts, isLoading: discountsLoading } = useDiscounts();
 
   return (
     <GlobalLayout pageTitle="Boutique">
-      <ShopCardsWrapper>
-        {discounts?.map((discount, index) => {
-          return (
-            <View key={`${discount.name}+${index}`}>
-              <ShopCard discount={discount} />
+      {discountsLoading && (
+        <ActivityIndicator color={theme.colors.brand.primary.base} />
+      )}
 
-              <Spacer axis="vertical" size={1} />
-            </View>
-          );
-        })}
-      </ShopCardsWrapper>
+      {!discountsLoading && (
+        <ShopCardsWrapper>
+          {discounts?.map((discount, index) => {
+            return (
+              <View key={`${discount.name}+${index}`}>
+                <ShopCard discount={discount} />
+
+                <Spacer axis="vertical" size={1} />
+              </View>
+            );
+          })}
+        </ShopCardsWrapper>
+      )}
     </GlobalLayout>
   );
 }
