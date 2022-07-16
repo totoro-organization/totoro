2f3122539300d8af96e51e0fae70ac5c0ee4f724
MaevaWolff <maeva.wolff@hetic.net>
2022-07-16 20:58:04 +0200

    feat(api-cypress): add login command to cypress

--- api/cypress/support/commands.js
+++ api/cypress/support/commands.js
@@ -1,25 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const { API_ROUTES } = require("./routes");
+import loginEmail from "../fixtures/user/login-email.json";
+
+Cypress.Commands.add("login", () => {
+  cy.request({
+    method: "POST",
+    url: API_ROUTES.LOGIN,
+    body: loginEmail,
+    headers: {
+      "content-type": "application/json",
+      app_id: "ad7258a1-bc27-4b04-882b-eeca86afcc7e",
+    },
+  }).then((response) => {
+    window.localStorage.setItem("token", response.body.token);
+    expect(response.status).eq(201);
+  });
+});
