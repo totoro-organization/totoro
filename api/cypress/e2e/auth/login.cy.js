d6557f3ed1fdbb3e41dbdc3550cadf16b69deeb0
MaevaWolff <maeva.wolff@hetic.net>
2022-07-16 19:59:17 +0200

    refactor(api-cypress): add fake data to fixture file

--- api/cypress/e2e/auth/login.cy.js
+++ api/cypress/e2e/auth/login.cy.js
@@ -1,21 +1,14 @@
 import { API_ROUTES } from "../../support/routes";
-
-const FAKE_USER_EMAIL = "mae-test@mae.mae";
-const FAKE_USER_USERNAME = "maem";
-const FAKE_USER_PASSWORD = "123456";
+import loginUserEmail from "../../fixtures/user/login-email.json";
+import loginUserName from "../../fixtures/user/login-username.json";
 
 // TODO: Add test when the user received an error.
 describe("POST /login", () => {
   it("user connects with our email", () => {
-    const body = {
-      emailOrUsername: FAKE_USER_EMAIL,
-      password: FAKE_USER_PASSWORD,
-    };
-
     cy.request({
       method: "POST",
       url: API_ROUTES.LOGIN,
-      body: body,
+      body: loginUserEmail,
       headers: {
         "content-type": "application/json",
         app_id: "ad7258a1-bc27-4b04-882b-eeca86afcc7e",
@@ -26,15 +19,10 @@ describe("POST /login", () => {
   });
 
   it("user connects with our username", () => {
-    const body = {
-      emailOrUsername: FAKE_USER_USERNAME,
-      password: FAKE_USER_PASSWORD,
-    };
-
     cy.request({
       method: "POST",
       url: API_ROUTES.LOGIN,
-      body: body,
+      body: loginUserName,
       headers: {
         "content-type": "application/json",
         app_id: "ad7258a1-bc27-4b04-882b-eeca86afcc7e",
