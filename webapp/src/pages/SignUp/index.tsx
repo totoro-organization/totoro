1ae4ccd9e89af09789afd968ab1e3dca71c26eef
tlugat <teo.lugat@hetic.net>
2022-07-16 13:17:53 +0200

    feat-webapp: add custom form elements w react hook form and yup to signup form

--- webapp/src/pages/SignUp/index.tsx
+++ webapp/src/pages/SignUp/index.tsx
@@ -1,7 +1,6 @@
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import { NavLink } from 'react-router-dom';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
@@ -10,141 +9,150 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
 import { useSession } from 'src/hooks/useSession';
-
-function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import { FormProvider, useForm } from 'react-hook-form';
+import { FormContainer, FormDatePicker, FormTextField } from 'src/components/forms';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { SignupSchema } from './Signup.schema';
+import { format } from 'date-fns';
+import { SignUpData } from 'src/models';
+import Copyright from 'src/components/Copyright';
 
 const theme = createTheme();
 
+interface SignupFieldTypes {
+  email: string,
+  password: string,
+  firstname: string,
+  lastname: string,
+  username: string,
+  birthday: Date
+}
+
 export default function SignUp() {
+  const methods = useForm<SignupFieldTypes>({
+    resolver: yupResolver(SignupSchema)
+  });
 
   const { signup, loading } = useSession();
 
-  const [date, setDate] = useState<Date | null>(null);
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-
-    signup({
-      email: data.get('email'),
-      password: data.get('password'),
-      firstname: data.get('firstname'),
-      lastname: data.get('lastname'),
-      username: data.get('username'),
-      birthday: date,
-    });
+  const onSubmit = (formData: SignupFieldTypes) => {
+    const data: SignUpData = {
+      email: formData.email,
+      password: formData.password,
+      firstname: formData.firstname,
+      lastname: formData.lastname,
+      username: formData.username,
+      birthday: format(new Date(formData.birthday), 'yyyy-MM-dd')
+    };
+    signup(data);
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth="sm">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
+            alignItems: 'center'
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            S'inscrire
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstname"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastname"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="username"
-                  label="Username"
-                  type="username"
-                  id="username"
-                  autoComplete="username"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              {/* <Grid item xs={12}>
+          <FormProvider {...methods}>
+            <FormContainer
+              onSubmit={methods.handleSubmit(onSubmit)}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <FormTextField
+                    autoComplete="given-name"
+                    name="firstname"
+                    fullWidth
+                    id="firstName"
+                    label="Prénom"
+                    defaultValue=""
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormTextField
+                    fullWidth
+                    id="lastName"
+                    label="Nom"
+                    name="lastname"
+                    defaultValue=""
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormTextField
+                    fullWidth
+                    name="username"
+                    label="Pseudo"
+                    type="username"
+                    id="username"
+                    defaultValue=""
+                    autoComplete="username"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormTextField
+                    fullWidth
+                    id="email"
+                    label="Adresse mail"
+                    name="email"
+                    defaultValue=""
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormTextField
+                    fullWidth
+                    name="password"
+                    label="Mot de passe"
+                    type="password"
+                    id="password"
+                    defaultValue=""
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormDatePicker label="Date de naissance" name="birthday" />
+                </Grid>
+
+                {/* <Grid item xs={12}>
                 <FormControlLabel
                   control={<Checkbox value="allowExtraEmails" color="primary" />}
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
               </Grid> */}
-            </Grid>
-            <Button
-              disabled={loading}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
+              </Grid>
+              <Button
+                disabled={loading}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                S'inscrire
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
                   <Link to="/login" component={NavLink} variant="body2">
                     Déjà inscrit ? Je me connecte
                   </Link>
+                </Grid>
               </Grid>
-            </Grid>
-          </Box>
+            </FormContainer>
+          </FormProvider>
         </Box>
         <Copyright sx={{ mt: 5 }} />
       </Container>
