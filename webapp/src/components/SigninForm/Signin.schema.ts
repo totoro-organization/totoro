de7d2a486dffea43b876c372f50c4b50df30d27f
tlugat <teo.lugat@hetic.net>
2022-07-17 18:28:16 +0200

    feat-webapp: update signin form schema

--- webapp/src/components/SigninForm/Signin.schema.ts
+++ webapp/src/components/SigninForm/Signin.schema.ts
@@ -3,5 +3,5 @@ import { REQUIRED_MSG } from 'src/components/forms/validationConstants';
 
 export const SigninSchema = Yup.object({
   emailOrUsername: Yup.string().min(2).required(REQUIRED_MSG).trim(),
-  password: Yup.string().min(8).required(REQUIRED_MSG).trim(),
+  password: Yup.string().min(6).required(REQUIRED_MSG).trim(),
 }).required(REQUIRED_MSG);
