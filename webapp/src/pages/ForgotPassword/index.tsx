fad78e8335d156f9a63944083c3cef22bbb20bad
tlugat <teo.lugat@hetic.net>
2022-07-17 12:29:41 +0200

    feat-webapp: add heading to forgot password page

--- webapp/src/pages/ForgotPassword/index.tsx
+++ webapp/src/pages/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import ForgotPasswordForm from 'src/components/ForgotPasswordForm';
 
 const containerStyles = {
@@ -12,6 +12,12 @@ const containerStyles = {
 function ForgotPassword() {
   return (
     <Container sx={containerStyles} component="main" maxWidth="sm">
+      <Typography component="h1" variant="h2">
+        Oublier son mot de passe ça arrive même aux meilleurs !
+      </Typography>
+      <Typography component="h5" variant="subtitle1">
+       Ne t'en fais pas, donne moi ton email et si il correspond à un compte utilisateur, tu recevras un mail de confirmation sur la boîte mail associée.
+      </Typography>
       <ForgotPasswordForm />
     </Container>
   );
