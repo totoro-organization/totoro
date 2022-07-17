624ab873b97cf7c82ca9b77b699cd8a099ed2fff
tlugat <teo.lugat@hetic.net>
2022-07-17 13:02:43 +0200

    feat-webapp: forgot password page layout style

--- webapp/src/pages/ForgotPassword/index.tsx
+++ webapp/src/pages/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography } from '@mui/material';
+import { Container, styled, Typography } from '@mui/material';
 import ForgotPasswordForm from 'src/components/ForgotPasswordForm';
 
 const containerStyles = {
@@ -6,18 +6,28 @@ const containerStyles = {
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  rowGap: 8
+  rowGap: 4,
+  height: '100%'
 };
+
+const Heading = styled("div")({
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: 8,
+})
 
 function ForgotPassword() {
   return (
     <Container sx={containerStyles} component="main" maxWidth="sm">
-      <Typography component="h1" variant="h2">
+      <Heading>
+        <Typography component="h1" variant="h2">
         Oublier son mot de passe ça arrive même aux meilleurs !
       </Typography>
       <Typography component="h5" variant="subtitle1">
        Ne t'en fais pas, donne moi ton email et si il correspond à un compte utilisateur, tu recevras un mail de confirmation sur la boîte mail associée.
       </Typography>
+      </Heading>
+      
       <ForgotPasswordForm />
     </Container>
   );
