547f6682b822e84f4b96d9a3e10b21790cbaa531
tlugat <teo.lugat@hetic.net>
2022-07-18 16:27:34 +0200

    feat-bo: add applications settings page

---
+++ backoffice/src/pages/Applications/Settings/Applications/Applications.tsx
@@ -0,0 +1,52 @@
+import { Box } from '@mui/material';
+import ApplicationsTable from './ApplicationsTable';
+import { useApi } from 'src/hooks/useApi';
+import SuspenseLoader from 'src/components/SuspenseLoader';
+import TableWrapper from 'src/components/TableWrapper';
+import { styled } from '@mui/system';
+import { StatusEnum } from 'src/models/status';
+import { TableEnum } from 'src/models';
+import { API_ROUTES } from 'src/services/routes';
+
+
+const WrapperBox = styled(Box)(
+  ({ theme }) => `
+    display: flex;
+    flex-direction: column;
+    row-gap: ${theme.spacing(2)}
+`
+);
+
+function Applications() {
+
+  const { data: applications, loading  } = useApi(API_ROUTES.APPLICATIONS);
+
+  const statusOptions = [
+    {
+      id: StatusEnum.actived,
+      name: 'Actif'
+    },
+    {
+      id: StatusEnum.disabled,
+      name: "Inactif"
+    },
+    {
+      id: StatusEnum.deleted,
+      name: 'Supprimé'
+    },
+  ];
+
+  return (
+    <WrapperBox>
+      {
+        loading || !applications ? <SuspenseLoader/> : 
+        <TableWrapper table={TableEnum.applications} addButton url={API_ROUTES.TAGS} statusOptions={statusOptions} defaultItems={applications?.data}>
+          {/* @ts-ignore */}
+            <ApplicationsTable  />
+        </TableWrapper>
+      }
+    </WrapperBox>
+  );
+}
+
+export default Applications;
---
+++ backoffice/src/pages/Applications/Settings/Applications/index.tsx
@@ -0,0 +1,44 @@
+import { Helmet } from 'react-helmet-async';
+import PageTitle from 'src/components/PageTitle';
+import PageTitleWrapper from 'src/components/PageTitleWrapper';
+import {
+  Container,
+  Grid,
+} from '@mui/material';
+
+import Footer from 'src/components/Footer';
+import Applications from './Applications';
+
+
+function ApplicationSettings() {
+  
+  return (
+    <>
+      <Helmet>
+        <title>Paramétrage des applications</title>
+      </Helmet>
+      <PageTitleWrapper>
+        <PageTitle
+          heading="Applications"
+          subHeading="Modifier la liste des applications"
+        />
+      </PageTitleWrapper>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12}>
+            <Applications/>
+          </Grid>
+        </Grid>
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default ApplicationSettings;
