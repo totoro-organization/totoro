18a9d61985eb1125721c5917407344b673f96464
tlugat <teo.lugat@hetic.net>
2022-08-11 20:23:34 +0200

    feat-webapp: remove type from add organization form

--- webapp/src/components/AddOrganizationForm/AddOrganization.schema.ts
+++ webapp/src/components/AddOrganizationForm/AddOrganization.schema.ts
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { REQUIRED_MSG } from 'src/components/forms/validationConstants';
+import { PHONE_VALID_MSG, REQUIRED_MSG, SIRET_VALID_MSG } from 'src/components/forms/validationConstants';
+
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+const siretRegExp = /\d{14}/g;
 
 export const AddOrganizationSchema = Yup.object({
-  phone: Yup.number().min(10).max(10).required(REQUIRED_MSG),
-  type: Yup.string().oneOf(['siret', 'siren']),
-  typeValue: Yup.string().required(REQUIRED_MSG),
+  phone: Yup.string().matches(phoneRegExp, PHONE_VALID_MSG).required(REQUIRED_MSG),
+  siret: Yup.string().matches(siretRegExp, SIRET_VALID_MSG).required(REQUIRED_MSG),
   email: Yup.string().email().required(REQUIRED_MSG).trim(),
 }).required(REQUIRED_MSG);
--- webapp/src/components/AddOrganizationForm/index.tsx
+++ webapp/src/components/AddOrganizationForm/index.tsx
@@ -1,8 +1,8 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Button, Link } from '@mui/material';
+import { Button, Link, MenuItem } from '@mui/material';
 import { FormProvider, useForm } from 'react-hook-form';
 import { NavLink } from 'react-router-dom';
-import { FormContainer, FormTextField } from 'src/components/forms';
+import { FormContainer, FormSelect, FormTextField } from 'src/components/forms';
 import { useToast } from 'src/hooks/useToast';
 import { AddOrganizationData } from 'src/models/services';
 import { addItem } from 'src/services/common.service';
@@ -10,10 +10,9 @@ import { API_ROUTES } from 'src/services/routes';
 import { AddOrganizationSchema } from './AddOrganization.schema';
 
 interface AddOrganizationFieldTypes {
-  type: "siret" | "siren",
-  typeValue: string,
-  email: string,
-  phone: number
+  siret: string;
+  email: string;
+  phone: number;
 }
 
 function AddOrganizationForm() {
@@ -23,26 +22,27 @@ function AddOrganizationForm() {
   const { setToast } = useToast();
   const onSubmit = async (formData: AddOrganizationFieldTypes) => {
     const data: AddOrganizationData = {
-      type: formData.type,
-      typeValue: formData.typeValue,
+      siret: formData.siret,
       email: formData.email,
       phone: formData.phone
     };
-    const response = await addItem(API_ROUTES.ORGANIZATIONS, data);
-    if ('error' in response) {
-      setToast({
-        variant: 'error',
-        message: response.message,
-        duration: 6000
-      });
-      return;
-    }
-    setToast({
-      variant: 'success',
-      message: 'Votre demande à été envoyée avec succès. Vous serez notifié quand elle sera sera acceptée ou refusée',
-      duration: 8000
-    });
-
+    console.log(data);
+    
+    // const response = await addItem(API_ROUTES.ORGANIZATIONS, data);
+    // if ('error' in response) {
+    //   setToast({
+    //     variant: 'error',
+    //     message: response.message,
+    //     duration: 6000
+    //   });
+    //   return;
+    // }
+    // setToast({
+    //   variant: 'success',
+    //   message:
+    //     'Votre demande à été envoyée avec succès. Vous serez notifié quand elle sera sera acceptée ou refusée',
+    //   duration: 8000
+    // });
   };
 
   return (
@@ -52,10 +52,9 @@ function AddOrganizationForm() {
           defaultValue={''}
           margin="normal"
           fullWidth
-          name="typeValue"
-          label="N°SIREN/SIRET"
-          type="number"
-          id="typeValue"
+          name="siret"
+          label="N°SIRET"
+          id="siret"
           autoComplete="current-type"
         />
         <FormTextField
@@ -74,7 +73,7 @@ function AddOrganizationForm() {
           fullWidth
           name="phone"
           label="N° de téléphone"
-          type="phone"
+          type="tel"
           id="phone"
           autoComplete="current-phone"
         />
@@ -83,17 +82,10 @@ function AddOrganizationForm() {
           type="submit"
           fullWidth
           variant="contained"
-          sx={{ mt: 3, mb: 2 }}
+          sx={{ width: 'fit-content' }}
         >
-          Réinitialiser
+          Envoyer
         </Button>
-        <Link
-          component={NavLink}
-          to="/login"
-          variant="body2"
-        >
-          J'avais juste oublié ! Je me connecte
-        </Link>
       </FormContainer>
     </FormProvider>
   );
