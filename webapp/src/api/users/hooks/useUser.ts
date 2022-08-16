665bdecda9f4d0c84c000337afc75c2044add484
tlugat <teo.lugat@hetic.net>
2022-08-16 10:14:32 +0200

    refacto-webapp: add api users hooks and requests

---
+++ webapp/src/api/users/hooks/useUser.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { ApiResponse } from 'src/api/shared/interfaces';
+import { User } from 'src/models';
+import { getUser } from '../requests';
+import { useToast } from 'src/hooks/useToast';
+
+export type useUserResponse = {
+  data: ApiResponse<User>;
+  error: any;
+  loading: boolean;
+  getData: () => Promise<any>
+};
+
+export const useUser = (id: string): useUserResponse => {
+  const [data, setData] = useState<ApiResponse<User>>();
+  const [error, setError] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const { setToast } = useToast();
+
+  const getAPIData = async (): Promise<any> => {
+    if(error) setError(null);
+    try {
+      const response = await getUser(id);
+      if('error' in response) {
+        setError(response.message);
+        setToast({ variant: 'error', message: response.message, duration: 6000})
+        return;
+      }
+      setData(response);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if(!error) {
+      getAPIData();
+    }
+  }, []);
+
+  return { data, error, loading, getData: getAPIData };
+};
