6cd5ea7b7f8ee1eee7074540798b826cfa2a08a4
jarce Boukoro <jarce.boukoro@korian.fr>
2022-09-07 15:38:27 +0200

    fix create job

--- api/src/services/Jobs/controller.js
+++ api/src/services/Jobs/controller.js
@@ -344,7 +344,7 @@ module.exports = {
 					var allValueTags = [];
 					for (var i=0; i < tagsJob.length; i++) {
 						var valueObj = {
-							id: `"${uuidv4()}"`,
+							id: `'${uuidv4()}'`,
 							tag_id: tagsJob[i],
 							jobs_id: result.id
 						};
@@ -358,7 +358,7 @@ module.exports = {
 					
 							for (index = 0, len = files.length; index < len; ++index) {
 								var valueObj = {
-									id: `"${uuidv4()}"`,
+									id: `'${uuidv4()}'`,
 									jobs_id: result.id,
 									original_name: files[index].originalname,
 									type: files[index].mimetype,
@@ -430,7 +430,7 @@ module.exports = {
 				var allValueTags = [];
 				for (var i=0; i < tagsJob.length; i++) {
 					var valueObj = {
-						id: `"${uuidv4()}"`,
+						id: `'${uuidv4()}'`,
 						tag_id: tagsJob[i],
 						jobs_id: id
 					};
